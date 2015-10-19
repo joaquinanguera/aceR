c54137a9b923ef42bd6f79c7db2b195a014717c8
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-10-18 23:08:32 -0700

    load uniquely structured individual files for testing

--- scripts/load_csv_google_drive.R
+++ scripts/load_csv_google_drive.R
@@ -2,7 +2,7 @@
 
 library(aceR)
 
-raw_data_path = "~/Google Drive/ACE Studies_Raw Data/"
+raw_data_path = "~/Google Drive/ACE Studies_Raw Data/Adapativity"
 setwd(raw_data_path)
 
 raw_files = files_in_directory()
@@ -13,3 +13,12 @@ for (raw_file in raw_files) {
   dat = read_raw_csv(raw_file)
   out = plyr::rbind.fill(out, dat)
 }
+
+# load individual files - each with their own unique structure
+
+boxed = read_raw_csv("Raw Data/Boxed-2.csv")
+
+flanker_one_block = read_raw_csv("Raw Data/Flanker-46.csv")
+
+flanker_multi_block = read_raw_csv("Raw Data/Flanker-5.csv") # throws error
+
