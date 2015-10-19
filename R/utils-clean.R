b9dcf2d0028335857718d68bcf42f2a2717ee837
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-10-18 22:24:31 -0700

    add remove empty rows function

--- R/utils-clean.R
+++ R/utils-clean.R
@@ -3,9 +3,18 @@
 #'
 #' @export
 #' @param dat a data frame
-#' @return Returns a data frame where the empty columns 
-#'  have been removed
+#' @return Returns a data frame where the empty columns have been removed
 
 remove_empty_cols <- function(dat) {
   return(dat[!sapply(dat, function(x) all(x == ""))])
+}
+
+#' Remove empty rows from a data frame.
+#'
+#' @export
+#' @param dat a data frame
+#' @return Returns a data frame where the empty rows have been removed
+
+remove_empty_rows <- function(dat) {
+  return(dat[rowSums(is.na(dat)) != ncol(dat),])
 }
