91ebe8a750514354d725a911ff51c6f32bfdfdb5
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-10-21 21:30:49 -0700

    use new 01 for dev

--- scripts/load_csv_google_drive.R
+++ scripts/load_csv_google_drive.R
@@ -2,7 +2,7 @@
 
 library(aceR)
 
-raw_data_path = "~/Google Drive/ACE Studies_Raw Data/Adaptivity"
+raw_data_path = "~/Google Drive/ACE Studies_Raw Data/Adaptivity/Raw Data Files/01"
 setwd(raw_data_path)
 
 raw_files = files_in_directory()
@@ -10,15 +10,7 @@ raw_files = files_in_directory()
 out = data.frame()
 for (raw_file in raw_files) {
   print(raw_file)
-  dat = read_raw_csv(raw_file)
+  dat = read_raw_csv(raw_file, TRUE)
   out = plyr::rbind.fill(out, dat)
 }
-
-# load individual files - each with their own unique structure
-
-boxed = read_raw_csv("Raw Data/Boxed-2.csv")
-
-flanker_one_block = read_raw_csv("Raw Data/Flanker-46.csv")
-
-flanker_multi_block = read_raw_csv("Raw Data/Flanker-5.csv") # throws error
 
