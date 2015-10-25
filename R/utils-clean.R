cf08fb71f8187cb93f674ee01a670ba61e602709
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-10-25 15:19:14 -0700

    add row utility functions

--- R/utils-clean.R
+++ R/utils-clean.R
@@ -18,3 +18,33 @@ remove_empty_cols <- function(dat) {
 remove_empty_rows <- function(dat) {
   return(dat[rowSums(is.na(dat)) != ncol(dat),])
 }
+
+#' Remove specified rows from a data frame
+#'
+#' @export
+#' @param dat a data frame
+#' @param rows a vector containing the names of the the rows to remove
+#' @param reset reset the row numbers?
+#' @return Returns a data frame where the specified rows have been removed
+
+remove_rows <- function(dat, rows = c(), reset = TRUE) {
+  if (length(rows) == 0) {
+    return (dat)
+  }
+  dat = dat[-rows, ] 
+  if (reset) {
+    row.names(dat) = NULL
+  }
+  return (dat)
+}
+
+#' Numeric row names
+#'
+#' @export
+#' @param dat a data frame
+#' @return Returns numeric row names
+
+numeric_row_names <- function(dat) {
+  rows = row.names(dat)
+  return (as.numeric(as.character(rows)))
+}
