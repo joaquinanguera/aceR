16281f9a284b3aadf936973350f29fc960eb387f
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-10-28 22:13:52 -0700

    add a bunch of individual files for testing raw csv variations

--- scripts/load_csv_google_drive.R
+++ scripts/load_csv_google_drive.R
@@ -2,7 +2,7 @@
 
 library(aceR)
 
-raw_data_path = "~/Google Drive/ACE Studies_Raw Data/Adaptivity/Raw Data Files/01"
+raw_data_path = "~/Google Drive/ACE Studies_Raw Data/Adaptivity/Raw Data Files"
 setwd(raw_data_path)
 
 raw_files = files_in_directory()
@@ -13,4 +13,32 @@ for (raw_file in raw_files) {
   dat = read_raw_csv(raw_file, TRUE)
   out = plyr::rbind.fill(out, dat)
 }
+
+
+# variations
+
+dat = read_raw_csv("01/Boxed_01.csv")
+
+dat = read_raw_csv("01/BRT_01.csv")
+
+dat = read_raw_csv("16/Boxed_16.csv")
+
+dat = read_raw_csv("16/Stroop_16.csv")
+
+dat = read_raw_csv("16/Flanker_16.csv") 
+
+dat = read_raw_csv("12/Stroop_12.csv")
+
+dat = read_raw_csv("16/Discrimination_16.csv")
+
+dat = read_raw_csv("03/Discrimination_03.csv")
+
+na_locf <- function(df, cols) {
+  fill_last = sapply(df[cols], function(x) {
+    x[x == ""] = NA
+    return(zoo::na.locf(x))
+  })
+  return(fill_last)
+}
+
 
