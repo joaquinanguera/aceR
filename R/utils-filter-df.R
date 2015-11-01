51e0fab27f4fcc6ac33e4f201be18ad56f230fce
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-01 09:44:15 -0800

    reorganize filter functions

--- R/utils-filter-df.R
+++ R/utils-filter-df.R
@@ -2,49 +2,49 @@
 #' Remove empty columns from a data frame.
 #'
 #' @export
-#' @param dat a data frame
+#' @param df a data frame
 #' @return Returns a data frame where the empty columns have been removed
 
-remove_empty_cols <- function(dat) {
-  return(dat[!sapply(dat, function(x) all(x == ""))])
+remove_empty_cols <- function(df) {
+  return(df[!sapply(df, function(x) all(x == ""))])
 }
 
 #' Remove empty rows from a data frame.
 #'
 #' @export
-#' @param dat a data frame
+#' @inheritParams remove_empty_cols
 #' @return Returns a data frame where the empty rows have been removed
 
-remove_empty_rows <- function(dat) {
-  return(dat[rowSums(is.na(dat)) != ncol(dat),])
+remove_empty_rows <- function(df) {
+  return(df[rowSums(is.na(df)) != ncol(df),])
 }
 
 #' Remove specified rows from a data frame
 #'
 #' @export
-#' @param dat a data frame
+#' @inheritParams remove_empty_cols
 #' @param rows a vector containing the names of the the rows to remove
 #' @param reset reset the row numbers?
 #' @return Returns a data frame where the specified rows have been removed
 
-remove_rows <- function(dat, rows = c(), reset = TRUE) {
+remove_rows <- function(df, rows = c(), reset = TRUE) {
   if (length(rows) == 0) {
-    return (dat)
+    return (df)
   }
-  dat = dat[-rows, ] 
+  df = df[-rows, ] 
   if (reset) {
-    row.names(dat) = NULL
+    row.names(df) = NULL
   }
-  return (dat)
+  return (df)
 }
 
 #' Numeric row names
 #'
 #' @export
-#' @param dat a data frame
+#' @inheritParams remove_empty_cols
 #' @return Returns numeric row names
 
-numeric_row_names <- function(dat) {
-  rows = row.names(dat)
+numeric_row_names <- function(df) {
+  rows = row.names(df)
   return (as.numeric(as.character(rows)))
 }
