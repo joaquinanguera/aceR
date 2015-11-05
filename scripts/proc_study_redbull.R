b3b60d43f4b864fecf6eb59ac692eef9532414da
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-04 21:35:44 -0800

    simple script for reading & processing rebull boxed data

---
+++ scripts/proc_study_redbull.R
@@ -0,0 +1,14 @@
+# script for processing redbull data
+
+rm(list = ls())
+
+library(aceR)
+
+path = "~/Google Drive/ACE Studies_Raw Data/RedBull/Raw Data"
+
+# load & process boxed dat
+boxed = read_raw_csv_in_directory(path = path, pattern = "Boxed")
+boxed_proc = module_boxed(boxed)
+
+setwd("~/Desktop")
+write.csv(boxed_proc, "redbull_boxed_v1.csv")
