5fdaf57de3768b34b8773dece8dabb9bc3b79ae7
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-08 18:37:12 -0800

    add basic docs

--- R/stats-proc-modules.R
+++ R/stats-proc-modules.R
@@ -1,5 +1,20 @@
 
+#' Process ACE data by module
+#'
+#' Calculates summary statistics by module.
+#'
+#' @section Assumptions:
+#' Assumes the column \emph{module} exists in the \code{\link{data.frame}}.
+#'
 #' @export
+#' @param df a \code{\link{data.frame}} containing formatted ACE data. 
+#'
+#' This includes data loaded with the following methods: 
+#' \enumerate{
+#'   \item \code{\link{read_raw_csv_in_directory}}
+#' }
+#' @return Returns summary statistics for every unique module included in the 
+#'  data. Throws warnings for modules with undefined methods.
 
 proc_by_module <- function(df) {
   all_mods = subset_by_col(df, "module")
