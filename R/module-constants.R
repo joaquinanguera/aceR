2101b8727bf8ca7e07b8fdaec84065dad1d84668
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-08 18:49:54 -0800

    defined some constants for processing user data by module

---
+++ R/module-constants.R
@@ -0,0 +1,9 @@
+
+#' ACE module constants
+#'
+#' @keywords internal
+#' @name ace_module_constants
+NULL
+
+#' @name ace_module_constants
+PARTICIPANT_BY_GROUP <- c("participant_id", "group")
--- R/module-module-boxed.R
+++ R/module-module-boxed.R
@@ -3,7 +3,7 @@
 #' @name ace_procs
 
 module_boxed <- function(df) {
-  rt = apply_stats(df, c("participant_id", "group"), "response_time", ace_descriptive_statistics)
-  acc = apply_stats(df, c("participant_id", "group"), "correct_button", ace_descriptive_statistics)
+  rt = apply_stats(df, PARTICIPANT_BY_GROUP, "response_time", ace_descriptive_statistics)
+  acc = apply_stats(df, PARTICIPANT_BY_GROUP, "correct_button", ace_descriptive_statistics)
   return (apply_stats_transform(list(rt, acc), "group", "participant_id"))
 }
