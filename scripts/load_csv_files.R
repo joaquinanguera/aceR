d2f2d092ef83339789d64b20a865a41328c77d71
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-08 20:06:38 -0800

    delete redundant script

--- scripts/load_csv_files.R
+++
@@ -1,8 +0,0 @@
-# example script for loading all raw csv data in a directory
-
-library(aceR)
-
-raw_data_path = "~/Google Drive/ACE Studies_Raw Data/16 Person Study/Raw Data"
-setwd(raw_data_path)
-
-dat = read_raw_csv_in_directory()
