aa26d6443c7d4a16331a5202a69ae1b7ab1699d3
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-10 23:01:27 -0800

    use constants to refer to columns

--- R/module-module-boxed.R
+++ R/module-module-boxed.R
@@ -3,8 +3,8 @@
 #' @name ace_procs
 
 module_boxed <- function(df) {
-  rt = apply_stats(x = df, y = PBG, col = "response_time", FUN = ace_descriptive_statistics, suffix = "overall")
-  acc = apply_stats(x = df, y = PBG, col = "correct_button", FUN = ace_descriptive_statistics, suffix = "overall")
-  rt_by_acc = apply_stats(x = df, y = PBG, col = "response_time", factor = "correct_button", FUN = ace_descriptive_statistics_by_group)
+  rt = apply_stats(x = df, y = PBG, col = COL_RT, FUN = ace_descriptive_statistics, suffix = "overall")
+  acc = apply_stats(x = df, y = PBG, col = COL_ACC, FUN = ace_descriptive_statistics, suffix = "overall")
+  rt_by_acc = apply_stats(x = df, y = PBG, col = COL_RT, factor = "correct_button", FUN = ace_descriptive_statistics_by_group)
   return (apply_stats_transform(list(rt, acc, rt_by_acc), "group", "participant_id"))
 }
