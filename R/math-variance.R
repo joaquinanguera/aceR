493a866cc3189d73ca10211540dba58408d530e1
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-10 22:12:07 -0800

    add variance by_group methods

--- R/math-variance.R
+++ R/math-variance.R
@@ -11,3 +11,15 @@ ace_se <- function(x) {
   vals = na.omit(to_numeric(x))
   return (sqrt(var(vals)/length(vals)))
 } 
+
+#' @keywords internal
+
+ace_sd_by_group <- function(x, y) {
+  return (ace_apply_by_group(x, y, ace_sd))
+}
+
+#' @keywords internal
+
+ace_se_by_group <- function(x, y) {
+  return (ace_apply_by_group(x, y, ace_se))
+}
