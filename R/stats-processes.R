f7b39481e7f3cd2ed56fabcd32f7b9edb0d29cc9
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-10 22:11:12 -0800

    add ace_descriptive_statistics_by_group function

--- R/stats-processes.R
+++ R/stats-processes.R
@@ -7,3 +7,11 @@ ace_descriptive_statistics <- function(x) {
   averages = c(mean = ace_mean(x), median = ace_median(x))
   return (c(variance, summary, averages))
 }
+
+#' @keywords internal 
+
+ace_descriptive_statistics_by_group <- function(x, y) {
+  var_by_group = c(sd = ace_sd_by_group(x, y), se = ace_se_by_group(x, y))
+  avg_by_group = c(mean = ace_mean_by_group(x, y), median = ace_median_by_group(x, y))
+  return (c(var_by_group, avg_by_group))
+}
