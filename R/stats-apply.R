53d606bb2f09ee30a4e17cd8bbbee8ba9e912297
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-10 22:15:01 -0800

    support by_group applying so we can group participant data 1 more level

--- R/stats-apply.R
+++ R/stats-apply.R
@@ -1,13 +1,22 @@
 
 #' @keywords internal
 
-apply_stats <- function(x, y, col, FUN, ...){
+apply_stats <- function(x, y, col, FUN, factor = NULL, ...){
+  if (length(y) != 2) {
+    stop("y must be length of 2")
+  }
+  by_factor = !missing(factor)
   z = plyr::ddply(x, y, .fun = function(xx) { 
     yy = xx[ ,col]
-    return (FUN(yy, ...))
+    if (!by_factor) {
+      return (FUN(yy, ...))
+    } else {
+      gg = xx[ ,factor]
+      return (as.data.frame(FUN(yy, gg, ...)))
+    }
   })
   ind = (length(y) + 1):length(z)
-  names(z)[ind] = sapply(names(z)[ind], function (n) paste(n, col, sep = "_"))
+  names(z)[ind] = sapply(names(z)[ind], function (n) paste(col, n, sep = "_"))
   return(z)
 }
 
@@ -17,4 +26,15 @@ apply_stats_transform <- function(proc_list, timevar, idvar, ...) {
   proc_merge = reshape::merge_recurse(proc_list)
   proc_reshaped = stats::reshape(proc_merge, timevar = timevar, idvar = idvar, direction = "wide", ...) 
   return (proc_reshaped)
+}
+
+#' @keywords internal
+
+ace_apply_by_group <- function(x, y, FUN) {
+  group = replace_blanks(y, NA)
+  agg = aggregate(list(x), list(group), FUN = FUN, simplify = TRUE)
+  out = data.frame(t(agg[2]))
+  names(out) = unlist(agg[1])
+  row.names(out) <- NULL
+  return (out)
 }
