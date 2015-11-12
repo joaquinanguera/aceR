bd2a59f54c310037b79b3f0d5258f31484e520c9
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-11 20:24:51 -0800

    comments + overall block stats

--- R/module-module-boxed.R
+++ R/module-module-boxed.R
@@ -3,8 +3,16 @@
 #' @name ace_procs
 
 module_boxed <- function(df) {
-  rt = apply_stats(x = df, y = PBG, col = COL_RT, FUN = ace_descriptive_statistics, suffix = "overall")
-  acc = apply_stats(x = df, y = PBG, col = COL_ACC, FUN = ace_descriptive_statistics, suffix = "overall")
-  rt_by_acc = apply_stats(x = df, y = PBG, col = COL_RT, factor = COL_ACC, FUN = ace_descriptive_statistics_by_group)
-  return (apply_stats_transform(list(rt, acc, rt_by_acc), "group", "participant_id"))
+  # overall
+  rt_overall = apply_stats(x = df, y = c("participant_id"), col = COL_RT, FUN = ace_descriptive_statistics, suffix = "overall")
+  acc_overall = apply_stats(x = df, y = c("participant_id"), col = COL_ACC, FUN = ace_descriptive_statistics, suffix = "overall")
+  # by feature
+  rt_by_feature = apply_stats(x = df, y = PBG, col = COL_RT, FUN = ace_descriptive_statistics, suffix = "overall")
+  acc_by_feature = apply_stats(x = df, y = PBG, col = COL_ACC, FUN = ace_descriptive_statistics, suffix = "overall")
+  # by feature & accuracy
+  rt_by_featute_and_acc = apply_stats(x = df, y = PBG, col = COL_RT, factor = COL_ACC, FUN = ace_descriptive_statistics_by_group)
+  # merge
+  proc_apply = apply_stats_transform(list(rt_by_feature, acc_by_feature, rt_by_featute_and_acc), "group", "participant_id")
+  proc_all = data.frame(rt_overall, acc_overall, proc_apply)
+  return (proc_all)
 }
