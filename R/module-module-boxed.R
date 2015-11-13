98d5d77c7b34d8f2fcf539d4fdbcf5da1e6c34e2
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-12 22:34:20 -0800

    calculate turns by feature for boxed module

--- R/module-module-boxed.R
+++ R/module-module-boxed.R
@@ -3,22 +3,28 @@
 #' @name ace_procs
 
 module_boxed <- function(df) {
+  
   # overall
   rw_overall = apply_stats(x = df, y = c("participant_id"), col = COL_RW, FUN = ace_descriptive_statistics, suffix = "overall")
   rt_overall = apply_stats(x = df, y = c("participant_id"), col = COL_RT, FUN = ace_descriptive_statistics, suffix = "overall")
   acc_overall = apply_stats(x = df, y = c("participant_id"), col = COL_ACC, FUN = ace_descriptive_statistics, suffix = "overall")
+  
   # by feature
   rw_by_feature = apply_stats(x = df, y = PBG, col = COL_RW, FUN = ace_descriptive_statistics, suffix = "overall")
   rt_by_feature = apply_stats(x = df, y = PBG, col = COL_RT, FUN = ace_descriptive_statistics, suffix = "overall")
   acc_by_feature = apply_stats(x = df, y = PBG, col = COL_ACC, FUN = ace_descriptive_statistics, suffix = "overall")
+  turns_by_feature = apply_stats(x = df, y = PBG, col = COL_RW, factor = COL_ACC, FUN = ace_average_turns,  suffix = "overall")
+  
   # by feature & accuracy
   rt_by_featute_and_acc = apply_stats(x = df, y = PBG, col = COL_RT, factor = COL_ACC, FUN = ace_descriptive_statistics_by_group)
+  
   # merge
   proc_apply = apply_stats_transform(
-    list(rw_by_feature, rt_by_feature, acc_by_feature, rt_by_featute_and_acc), 
+    list(rw_by_feature, rt_by_feature, acc_by_feature, rt_by_featute_and_acc, turns_by_feature), 
     "group", "participant_id")
   proc_all = multi_merge(
     list(rw_overall, rt_overall, acc_overall, proc_apply), 
     by = "participant_id")
+    
   return (proc_all)
 }
