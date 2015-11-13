a85d7abfe677694b32d7345ba8e214d59d257a3e
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-12 20:22:48 -0800

    response window summary stats for boxed

--- R/module-module-boxed.R
+++ R/module-module-boxed.R
@@ -4,15 +4,21 @@
 
 module_boxed <- function(df) {
   # overall
+  rw_overall = apply_stats(x = df, y = c("participant_id"), col = COL_RW, FUN = ace_descriptive_statistics, suffix = "overall")
   rt_overall = apply_stats(x = df, y = c("participant_id"), col = COL_RT, FUN = ace_descriptive_statistics, suffix = "overall")
   acc_overall = apply_stats(x = df, y = c("participant_id"), col = COL_ACC, FUN = ace_descriptive_statistics, suffix = "overall")
   # by feature
+  rw_by_feature = apply_stats(x = df, y = PBG, col = COL_RW, FUN = ace_descriptive_statistics, suffix = "overall")
   rt_by_feature = apply_stats(x = df, y = PBG, col = COL_RT, FUN = ace_descriptive_statistics, suffix = "overall")
   acc_by_feature = apply_stats(x = df, y = PBG, col = COL_ACC, FUN = ace_descriptive_statistics, suffix = "overall")
   # by feature & accuracy
   rt_by_featute_and_acc = apply_stats(x = df, y = PBG, col = COL_RT, factor = COL_ACC, FUN = ace_descriptive_statistics_by_group)
   # merge
-  proc_apply = apply_stats_transform(list(rt_by_feature, acc_by_feature, rt_by_featute_and_acc), "group", "participant_id")
-  proc_all = multi_merge(list(rt_overall, acc_overall, proc_apply), by = "participant_id")
+  proc_apply = apply_stats_transform(
+    list(rw_by_feature, rt_by_feature, acc_by_feature, rt_by_featute_and_acc), 
+    "group", "participant_id")
+  proc_all = multi_merge(
+    list(rw_overall, rt_overall, acc_overall, proc_apply), 
+    by = "participant_id")
   return (proc_all)
 }
