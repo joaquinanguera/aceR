4bc4905b50146722c5182416da6d3bd279bb2886
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-15 00:29:13 -0800

    explicitly set turns_by_factor

--- R/module-modules.R
+++ R/module-modules.R
@@ -3,19 +3,19 @@
 #' @name ace_procs
 
 module_boxed <- function(df) {
-  return (proc_generic_module(df, COL_CORRECT_BUTTON, COL_CONDITION))
+  return (proc_generic_module(df, COL_CORRECT_BUTTON, COL_CONDITION, TRUE))
 }
 
 #' @keywords internal
 #' @name ace_procs
 
 module_brt <- function(df) {
-  return (proc_generic_module(df, COL_CORRECT_BUTTON, COL_CONDITION))
+  return (proc_generic_module(df, COL_CORRECT_BUTTON, COL_CONDITION, TRUE))
 }
 
 #' @keywords internal
 #' @name ace_procs
 
 module_discrimination <- function(df) {
-  return (proc_generic_module(df, COL_CORRECT_RESPONSE, "cue_type"))
+  return (proc_generic_module(df, COL_CORRECT_RESPONSE, "cue_type", FALSE))
 }
