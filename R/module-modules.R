21f0df6c4cf6e1891fb9a56c01d27015e93b848d
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-15 09:33:54 -0800

    uncomment flanker proc

--- R/module-modules.R
+++ R/module-modules.R
@@ -20,14 +20,12 @@ module_discrimination <- function(df) {
   return (proc_generic_module(df, COL_CORRECT_RESPONSE, "cue_type", FALSE))
 }
 
-# TODO: see about xls files
+#' @keywords internal
+#' @name ace_procs
 
-# #' @keywords internal
-# #' @name ace_procs
-# 
-# module_flanker <- function(df) {
-#   return (proc_generic_module(df, COL_CORRECT_BUTTON, COL_TRIAL_TYPE, TRUE))
-# }
+module_flanker <- function(df) {
+  return (proc_generic_module(df, COL_CORRECT_BUTTON, COL_TRIAL_TYPE, TRUE))
+}
 
 #' @keywords internal
 #' @name ace_procs
