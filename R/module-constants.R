d362a75729bbc8a80384712aef4981d9d1d09e6e
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-14 18:54:26 -0800

    flesh out standard proc methods so we can avoid copypasta

--- R/module-constants.R
+++ R/module-constants.R
@@ -10,3 +10,13 @@ PARTICIPANT_BY_PARTICIPANT <- c(COL_PID)
 
 #' @name ace_module_constants
 PARTICIPANT_BY_CONDITION <- c(COL_PID, COL_CONDITION)
+
+#' @keywords internal
+
+proc_standard <- function(df, variable, condition = TRUE) {
+  overall = apply_stats(x = df, y = PARTICIPANT_BY_PARTICIPANT, col = variable, FUN = ace_descriptive_statistics, suffix = "overall")
+  by_condition = apply_stats(x = df, y = PARTICIPANT_BY_CONDITION, col = variable, FUN = ace_descriptive_statistics, suffix = "overall")
+  by_condition_transform = stats::reshape(by_condition, timevar = COL_CONDITION, idvar = COL_PID, direction = "wide") 
+  proc = multi_merge(list(overall, by_condition_transform), by = COL_PID)
+  return(proc)    
+}
