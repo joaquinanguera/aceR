5b2f09cefec4c745bed5802a6bf4342213fbdcc1
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-14 21:05:01 -0800

    don't assume acc & conditions are always the same columns - use args instead

--- R/module-constants.R
+++ R/module-constants.R
@@ -8,19 +8,16 @@ NULL
 #' @name ace_module_constants
 PARTICIPANT_BY_PARTICIPANT <- c(COL_PID)
 
-#' @name ace_module_constants
-PARTICIPANT_BY_CONDITION <- c(COL_PID, COL_CONDITION)
-
 #' @keywords internal
 
-proc_generic_module <- function(df) {
+proc_generic_module <- function(df, col_acc, col_condition) {
   # standard
-  rt = proc_standard(df, COL_RT)
-  acc = proc_standard(df, COL_ACC)
-  rw = proc_standard(df, COL_RW)
+  rt = proc_standard(df, COL_RT, col_condition)
+  rw = proc_standard(df, COL_RW, col_condition)
+  acc = proc_standard(df, col_acc, col_condition)
   # specific
-  rt_acc = proc_standard_factor(df, COL_RT, COL_ACC, ace_descriptive_statistics_by_group)
-  turns = proc_standard_factor(df, COL_RT, COL_ACC, ace_average_turns)
+  rt_acc = proc_standard_factor(df, COL_RT, col_condition, col_acc, ace_descriptive_statistics_by_group)
+  turns = proc_standard_factor(df, COL_RT, col_condition, col_acc, ace_average_turns)
   # merge
   analy = list(rt, acc, rw, rt_acc, turns)
   merged = multi_merge(analy, by = COL_PID)
@@ -29,18 +26,18 @@ proc_generic_module <- function(df) {
 
 #' @keywords internal
 
-proc_standard <- function(df, variable) {
-  overall = apply_stats(x = df, y = PARTICIPANT_BY_PARTICIPANT, col = variable, FUN = ace_descriptive_statistics, suffix = "overall")
-  by_condition = apply_stats(x = df, y = PARTICIPANT_BY_CONDITION, col = variable, FUN = ace_descriptive_statistics)
-  by_condition_transform = stats::reshape(by_condition, timevar = COL_CONDITION, idvar = COL_PID, direction = "wide") 
+proc_standard <- function(df, variable, col_condition) {
+  overall = apply_stats(x = df, y = c(COL_PID), col = variable, FUN = ace_descriptive_statistics, suffix = "overall")
+  by_condition = apply_stats(x = df, y = c(COL_PID, col_condition), col = variable, FUN = ace_descriptive_statistics)
+  by_condition_transform = stats::reshape(by_condition, timevar = col_condition, idvar = COL_PID, direction = "wide") 
   proc = multi_merge(list(overall, by_condition_transform), by = COL_PID)
   return(proc)
 }
 
 #' @keywords internal
 
-proc_standard_factor <- function (df, variable, factor, FUN) {
-  proc = apply_stats(x = df, y = PARTICIPANT_BY_CONDITION, col = variable, factor = factor, FUN = FUN)
-  transform = stats::reshape(proc, timevar = COL_CONDITION, idvar = COL_PID, direction = "wide")
+proc_standard_factor <- function (df, variable, condition, factor, FUN) {
+  proc = apply_stats(x = df, y = c(COL_PID, condition), col = variable, factor = factor, FUN = FUN)
+  transform = stats::reshape(proc, timevar = condition, idvar = COL_PID, direction = "wide")
   return (transform)
 }
