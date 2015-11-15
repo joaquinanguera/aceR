57b86590f9ace77ee00589f4c8ba2bf459e0ad74
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-14 19:15:02 -0800

    flesh out another common method for processing user data

--- R/module-constants.R
+++ R/module-constants.R
@@ -13,10 +13,18 @@ PARTICIPANT_BY_CONDITION <- c(COL_PID, COL_CONDITION)
 
 #' @keywords internal
 
-proc_standard <- function(df, variable, condition = TRUE) {
+proc_standard <- function(df, variable) {
   overall = apply_stats(x = df, y = PARTICIPANT_BY_PARTICIPANT, col = variable, FUN = ace_descriptive_statistics, suffix = "overall")
   by_condition = apply_stats(x = df, y = PARTICIPANT_BY_CONDITION, col = variable, FUN = ace_descriptive_statistics)
   by_condition_transform = stats::reshape(by_condition, timevar = COL_CONDITION, idvar = COL_PID, direction = "wide") 
   proc = multi_merge(list(overall, by_condition_transform), by = COL_PID)
   return(proc)
+}
+
+#' @keywords internal
+
+proc_standard_factor <- function (df, variable, factor, FUN) {
+  proc = apply_stats(x = df, y = PARTICIPANT_BY_CONDITION, col = variable, factor = factor, FUN = FUN)
+  transform = stats::reshape(proc, timevar = COL_CONDITION, idvar = COL_PID, direction = "wide")
+  return (transform)
 }
