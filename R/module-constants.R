120bb3278f9d5269a02ad60d33ba15e63f3cf5df
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-14 19:00:37 -0800

    get rid of suffix in by_condition vars to reduce verbosity

--- R/module-constants.R
+++ R/module-constants.R
@@ -15,8 +15,8 @@ PARTICIPANT_BY_CONDITION <- c(COL_PID, COL_CONDITION)
 
 proc_standard <- function(df, variable, condition = TRUE) {
   overall = apply_stats(x = df, y = PARTICIPANT_BY_PARTICIPANT, col = variable, FUN = ace_descriptive_statistics, suffix = "overall")
-  by_condition = apply_stats(x = df, y = PARTICIPANT_BY_CONDITION, col = variable, FUN = ace_descriptive_statistics, suffix = "overall")
+  by_condition = apply_stats(x = df, y = PARTICIPANT_BY_CONDITION, col = variable, FUN = ace_descriptive_statistics)
   by_condition_transform = stats::reshape(by_condition, timevar = COL_CONDITION, idvar = COL_PID, direction = "wide") 
   proc = multi_merge(list(overall, by_condition_transform), by = COL_PID)
-  return(proc)    
+  return(proc)
 }
