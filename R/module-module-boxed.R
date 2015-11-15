24f5e5756abc00deca6f0b604437c823c66f35b3
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-14 19:16:58 -0800

    clean-up: use standard methods - NOTE: we're sacrificing speed for clarity

--- R/module-module-boxed.R
+++ R/module-module-boxed.R
@@ -3,27 +3,14 @@
 #' @name ace_procs
 
 module_boxed <- function(df) {
-  
-  # overall
-  rw_overall = apply_stats(x = df, y = PARTICIPANT_BY_PARTICIPANT, col = COL_RW, FUN = ace_descriptive_statistics, suffix = "overall")
-  rt_overall = apply_stats(x = df, y = PARTICIPANT_BY_PARTICIPANT, col = COL_RT, FUN = ace_descriptive_statistics, suffix = "overall")
-  acc_overall = apply_stats(x = df, y = PARTICIPANT_BY_PARTICIPANT, col = COL_ACC, FUN = ace_descriptive_statistics, suffix = "overall")
-  
-  # by feature
-  rw_by_feature = apply_stats(x = df, y = PARTICIPANT_BY_CONDITION, col = COL_RW, FUN = ace_descriptive_statistics, suffix = "overall")
-  rt_by_feature = apply_stats(x = df, y = PARTICIPANT_BY_CONDITION, col = COL_RT, FUN = ace_descriptive_statistics, suffix = "overall")
-  acc_by_feature = apply_stats(x = df, y = PARTICIPANT_BY_CONDITION, col = COL_ACC, FUN = ace_descriptive_statistics, suffix = "overall")
-  turns_by_feature = apply_stats(x = df, y = PARTICIPANT_BY_CONDITION, col = COL_RW, factor = COL_ACC, FUN = ace_average_turns,  suffix = "overall")
-  
-  # by feature & accuracy
-  rt_by_featute_and_acc = apply_stats(x = df, y = PARTICIPANT_BY_CONDITION, col = COL_RT, factor = COL_ACC, FUN = ace_descriptive_statistics_by_group)
-  
+  # standard
+  rt = proc_standard(df, COL_RT)
+  acc = proc_standard(df, COL_ACC)
+  rw = proc_standard(df, COL_RW)
+  # specific
+  rt_acc = proc_standard_factor(df, COL_RT, COL_ACC, ace_descriptive_statistics_by_group)
+  turns = proc_standard_factor(df, COL_RT, COL_ACC, ace_average_turns)
   # merge
-  proc_apply = apply_stats_transform(
-    list(rw_by_feature, rt_by_feature, acc_by_feature, rt_by_featute_and_acc, turns_by_feature), COL_CONDITION, COL_PID)
-  proc_all = multi_merge(
-    list(rw_overall, rt_overall, acc_overall, proc_apply), 
-    by = COL_PID)
-    
-  return (proc_all)
+  proc = multi_merge(list(rt, acc, rw, rt_acc, turns), by = COL_PID)
+  return (proc)
 }
