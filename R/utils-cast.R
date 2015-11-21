094e1dc5797f5ba7ee7aa0a81a775ec2f965e104
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-21 15:20:18 -0800

    map incorrect/correct to 0,1

--- R/utils-cast.R
+++ R/utils-cast.R
@@ -2,5 +2,6 @@
 #' @keywords internal
 
 to_numeric <- function(x) {
-  return(as.numeric(as.character(x)))
+  vals = plyr::mapvalues(x, from = c("incorrect", "correct"), to = c(0, 1), warn_missing = FALSE)
+  return(as.numeric(as.character(vals)))
 }
