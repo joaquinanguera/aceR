f1dbff77f2c0a43e6c74b33864c110a1375e2987
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-11-29 17:06:30 -0800

    add multi_fun method

---
+++ R/math-costs.R
@@ -0,0 +1,15 @@
+
+#' @keywords internal
+# TODO: this needs work
+
+multi_fun <- function(df, first_pattern, second_pattern, suffix, FUN) {
+  p1 = df[stringr::str_detect(names(df), first_pattern)]
+  p2 = df[stringr::str_detect(names(df), second_pattern)]
+  indices = seq(1:length(p1))
+  out = sapply(indices, function (x) {
+    return (apply(data.frame(p1[, x], p2[, x]), 1, FUN))
+  })
+  out = as.data.frame(out)
+  names(out) = sapply(names(p1), function(x) gsub(first_pattern, suffix, x))
+  return(out)
+}
