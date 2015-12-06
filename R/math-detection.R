dd1b9713e5bf0747fdce95f87235efbc7e15007c
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-12-05 18:18:22 -0800

    added method for calculating signal detection rates

--- R/math-detection.R
+++ R/math-detection.R
@@ -1,5 +1,38 @@
 
+#' @keywords internal
 
+ace_detection <- function(x, y) {
+  rejs = identify_correct_rejections(x, y)
+  types = plyr::mapvalues(rejs, warn_missing = FALSE,
+    from = c("correct_rejection", "miss", "false_alarm", "hit"), 
+    to = c("target", "target", "nontarget", "nontarget"))
+  types = na.omit(types)
+  num_targets = length(which(types == "target"))
+  num_nontargets = length(which(types == "nontarget"))
+  freq = as.data.frame(t(summary(rejs)))
+  cols = names(freq)
+  if (!("false_alarm" %in% cols)) freq$false_alarm = 0
+  if (!("miss" %in% cols)) freq$miss = 0
+  if (!("correct_rejection" %in% cols)) freq$correct_rejection = 0
+  if (!("hit" %in% cols)) freq$hit = 0
+  out = c()
+  if (num_targets > 0) {
+    out$correct_rejection_rate = freq$correct_rejection / num_targets
+    out$miss_rate = freq$miss / num_targets
+  } else {
+    out$correct_rejection_rate = NA
+    out$miss_rate = NA
+  }
+  if (num_nontargets > 0) {
+    out$false_alarm_rate = freq$false_alarm / num_nontargets
+    out$hit_rate = freq$hit / num_nontargets
+  } else {
+    out$false_alarm_rate = NA
+    out$hit_rate = NA
+  }
+  return (out)
+}
+  
 #' @keywords internal
 
 identify_correct_rejections <- function(response_window, reaction_time) {
