3f9d9c2df96ebde94be0c43a2eeae5103a354c82
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-12-15 23:09:59 -0800

    filter_out_vec method

--- R/utils-filter-vec.R
+++ R/utils-filter-vec.R
@@ -6,3 +6,10 @@ filter_vec <- function(vec = c(), pattern = NULL) {
   match = sapply(vec, function(x) return (grepl(pattern, x)))
   return (vec[which(match)])
 }
+
+#' @keywords internal
+
+filter_out_vec <- function(vec = c(), pattern = NULL) {
+  match = sapply(vec, function(x) return (!grepl(pattern, x)))
+  return (vec[which(match)])
+}
