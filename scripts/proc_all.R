b5a38f4295b9dd679c9af7e37844daede122bfc7
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-12-16 22:38:13 -0800

    more problematic subdirectories to ignore

--- scripts/proc_all.R
+++ scripts/proc_all.R
@@ -8,7 +8,7 @@ library(aceR)
 # ACE Raw Data (*Processed Data Omitted)
 setwd("~/Desktop/ACE Studies_Raw Data")
 
-subdirs_to_ignore = c("Brighten", "Remaining _Jyoti","Remaining Raw Data (Brighten, BBT-MT, India, 16p-Adaptivity)")
+subdirs_to_ignore = c("Brighten", "Remaining _Jyoti","Remaining Raw Data (Brighten, BBT-MT, India, 16p-Adaptivity)", "Dan's Raw ACE Data")
 files_to_ignore = c("AgileAcademic-IAN", "FlankerAllWithDate", "FlankerAll")
 to_ignore = c(subdirs_to_ignore, files_to_ignore)
 
