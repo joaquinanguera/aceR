9e3acfbd59921095979ac319d5b56d7dfb0c9fc2
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-12-24 13:45:19 -0800

    unsuccessful attempt at handling case where data is inconsistent across excel sheet

--- R/load-xls.R
+++ R/load-xls.R
@@ -8,14 +8,34 @@ load_excel <- function(file) {
   if (length(sheets) == 1) {
     return (load_sheet(wk, sheet = 1))
   }
-  out = data.frame()
+  dfs = list()
   for (sheet in sheets) {
     if (tolower(sheet) %in% valid_sheets) {
-      sh = load_sheet(wk, sheet = sheet)
-      out = plyr:::rbind.fill(out, sh)
+      dfs[[sheet]] = load_sheet(wk, sheet = sheet)
     }
   }
-  return (out)
+  df_lens = as.vector(sapply(dfs, length))
+  equal_cols = all_equal_in_vec(df_lens)
+  if (equal_cols) {
+    # handle normal case
+    out = flatten_df_list(dfs)
+  } else {
+    # handle the case where data across sheets is inconsistent
+    out = data.frame()
+    for (df in dfs) {
+      names(df) = df[identify_first_header(df), ]
+      out = plyr:::rbind.fill(out, df)
+    }
+    # clean up 
+    names(out) = NULL
+    out = data.frame(out)
+    sh_rows = identify_shifted_rows(out)
+    for (r in sh_rows) {
+      out[r, 3] = out[r, 2]
+      out[r, 2] = NA
+    }
+  }
+  return (data.frame(out))
 }
 
 #' @keywords internal
@@ -40,4 +60,21 @@ sheet_names <- function(wk) {
 
 is_excel <- function (filename) {
   return (grepl("xls", filename))
+}
+
+#' @keywords internal
+
+identify_first_header <- function(dat) {
+  df = replace_nas(dat, "")
+  matching_rows = df[df[1] != "" & df[2] != "" & df[3] != "", ]
+  row_names = numeric_row_names(matching_rows)
+  return (row_names[1])
+}
+
+#' @keywords internal
+
+identify_shifted_rows <- function (dat) {
+  df = replace_nas(dat, "")
+  matching_rows = df[df[1] == "" & df[2] != "" & df[3] == "", ]
+  return (numeric_row_names(matching_rows))
 }
