ba152474ee83b05787e44058935d09b34aa5434b
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-12-23 23:11:26 -0800

    add flatten_df_list method

--- R/utils-merge-df.R
+++ R/utils-merge-df.R
@@ -4,3 +4,13 @@
 multi_merge <- function(df_list = list(), ...) {
   return (Reduce(function(x, y) merge(x, y, all = TRUE, ...), df_list))
 }
+
+#' @keywords internal
+
+flatten_df_list <- function (df_list = list()) {
+  out = data.frame()
+  for (df in df_list) {
+    out = plyr:::rbind.fill(out, df)
+  }
+  return (out)
+}
