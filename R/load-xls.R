f81cfcc7377dbc5f01bad872f2ccf3e392fdc82b
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-12-24 14:53:58 -0800

    just return a df list

--- R/load-xls.R
+++ R/load-xls.R
@@ -14,28 +14,7 @@ load_excel <- function(file) {
       dfs[[sheet]] = load_sheet(wk, sheet = sheet)
     }
   }
-  df_lens = as.vector(sapply(dfs, length))
-  equal_cols = all_equal_in_vec(df_lens)
-  if (equal_cols) {
-    # handle normal case
-    out = flatten_df_list(dfs)
-  } else {
-    # handle the case where data across sheets is inconsistent
-    out = data.frame()
-    for (df in dfs) {
-      names(df) = df[identify_first_header(df), ]
-      out = plyr:::rbind.fill(out, df)
-    }
-    # clean up 
-    names(out) = NULL
-    out = data.frame(out)
-    sh_rows = identify_shifted_rows(out)
-    for (r in sh_rows) {
-      out[r, 3] = out[r, 2]
-      out[r, 2] = NA
-    }
-  }
-  return (data.frame(out))
+  return (dfs)
 }
 
 #' @keywords internal
@@ -60,21 +39,4 @@ sheet_names <- function(wk) {
 
 is_excel <- function (filename) {
   return (grepl("xls", filename))
-}
-
-#' @keywords internal
-
-identify_first_header <- function(dat) {
-  df = replace_nas(dat, "")
-  matching_rows = df[df[1] != "" & df[2] != "" & df[3] != "", ]
-  row_names = numeric_row_names(matching_rows)
-  return (row_names[1])
-}
-
-#' @keywords internal
-
-identify_shifted_rows <- function (dat) {
-  df = replace_nas(dat, "")
-  matching_rows = df[df[1] == "" & df[2] != "" & df[3] == "", ]
-  return (numeric_row_names(matching_rows))
 }
