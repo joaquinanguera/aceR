e160e329be6ace63d47f44c3c5d797254675b507
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-12-23 23:11:45 -0800

    add all_equal_in_vec method

--- R/utils-filter-vec.R
+++ R/utils-filter-vec.R
@@ -13,3 +13,9 @@ filter_out_vec <- function(vec = c(), pattern = NULL) {
   match = sapply(vec, function(x) return (!grepl(pattern, x)))
   return (vec[which(match)])
 }
+
+#' @keywords internal
+
+all_equal_in_vec <- function (vec = c()) {
+  return (length(unique(vec) == 1))
+}
