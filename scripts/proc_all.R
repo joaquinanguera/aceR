2c3cbd0ca4161e2ac216302f039fce4bb14d605d
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-12-28 12:56:03 -0800

    more files to ignore

--- scripts/proc_all.R
+++ scripts/proc_all.R
@@ -8,8 +8,28 @@ library(aceR)
 # ACE Raw Data (*Processed Data Omitted)
 setwd("~/Desktop/ACE Studies_Raw Data")
 
-subdirs_to_ignore = c("Brighten", "Remaining _Jyoti","Remaining Raw Data (Brighten, BBT-MT, India, 16p-Adaptivity)", "Dan's Raw ACE Data")
-files_to_ignore = c("AgileAcademic-IAN", "FlankerAllWithDate", "FlankerAll")
+subdirs_to_ignore = c(
+  "Brighten", 
+  "Remaining _Jyoti", 
+  "Remaining Raw Data (Brighten, BBT-MT, India, 16p-Adaptivity)", 
+  "Dan's Raw ACE Data")
+
+files_to_ignore = c(
+  "AgileAcademic-IAN", 
+  "AgileEnvironments_ACE_pre-assessment-rankings_only", 
+  "final_ae_rankings",
+  "FlankerAll", 
+  "FlankerAllWithDate", 
+  "FlankerCatchAllPost",
+  "FlankerCatchAllPre"
+  "SAATAll",
+  "SAATAllWithDate",
+  "SAATCatchAllPost",
+  "SAATCatchAllPre",
+  "SpatialSpanAll",
+  "SpatialSpanAllWithDate",
+  "SpatialSpanCatchAllPre")
+
 to_ignore = c(subdirs_to_ignore, files_to_ignore)
 
 standard_ace_dat = load_ace_bulk(exclude = to_ignore)
