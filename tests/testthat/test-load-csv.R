341f68b3bedc69ec08eed1423511a5e83516341f
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-12-31 15:08:56 -0800

    test loading sample boxed data & sample bad data

--- tests/testthat/test-load-csv.R
+++ tests/testthat/test-load-csv.R
@@ -1,1 +1,7 @@
 context("load raw csv data")
+
+sample_bad = paste(aceR_sample_data_path(), "bad-data.csv", sep = "/")
+sample_boxed = paste(aceR_sample_data_path(), "sample-boxed.csv", sep = "/")
+
+expect_warning(load_ace_file(sample_bad))
+expect_more_than(nrow(load_ace_file(sample_boxed)), 0)
