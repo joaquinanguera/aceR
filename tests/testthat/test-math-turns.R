313a386ac1e2d76cab3210d0f3f8f042f008cb87
josegallegos07 <jose.gallegos@brainplasticity.com>
2016-01-03 19:49:53 -0800

    test for identifying turns

--- tests/testthat/test-math-turns.R
+++ tests/testthat/test-math-turns.R
@@ -3,4 +3,16 @@ context("test turns")
 sample_boxed = paste(aceR_sample_data_path(), "sample-boxed.csv", sep = "/")
 boxed_proc = load_ace_file(sample_boxed)
 
-rws = boxed_proc$rw
+# note: for simplicity's sake, we're collapsing data into single block.
+
+rws = aceR:::to_numeric(boxed_proc$rw)
+accs = aceR:::to_numeric(boxed_proc$correct_button)
+turns = aceR:::identify_turns(accs)
+avg_turns = aceR:::ace_turns(rws, accs, n = 3)
+
+for (i in turns) {
+  m = accs[i]
+  n = accs[i - 1]
+  expect_equal(m, 1)
+  expect_equal(n, 0)
+}
