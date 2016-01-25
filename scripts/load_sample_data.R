4eaaec2ee9f9a9a0b20433db8625dbc9a8c1346e
josegallegos07 <jose.gallegos@brainplasticity.com>
2016-01-24 22:52:22 -0800

    load sample brt

--- scripts/load_sample_data.R
+++ scripts/load_sample_data.R
@@ -4,3 +4,7 @@ library("aceR")
 sample_boxed = paste(aceR_sample_data_path(), "sample-boxed.csv", sep = "/")
 dat = load_ace_file(sample_boxed)
 boxed_proc = proc_by_module(dat)$BOXED
+
+sample_brt = paste(aceR_sample_data_path(), "sample-brt.xlsx", sep = "/")
+dat_brt = load_ace_file(sample_brt)
+boxed_proc_brt = proc_by_module(dat_brt)
