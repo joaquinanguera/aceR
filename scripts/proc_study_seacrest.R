c0c93645ee8ec3001eaf8850773b2e47baed95ff
josegallegos07 <jose.gallegos@brainplasticity.com>
2016-02-09 20:33:04 -0800

    proc seacrest script

---
+++ scripts/proc_study_seacrest.R
@@ -0,0 +1,14 @@
+# script for processing seacrest data
+
+rm(list = ls())
+
+library(aceR)
+
+setwd("~/Desktop/Raw ACE Data Sea Crest School")
+
+# load & process dat
+dat = load_ace_bulk()
+proc = proc_by_module(dat, TRUE)
+
+setwd("~/Desktop/process")
+export_csv(proc)
