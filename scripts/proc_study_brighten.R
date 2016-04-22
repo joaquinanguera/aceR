d6d6d3da8811628e0f3a27fcbfca129623c66dc5
Jose Gallegos <josegallegos07@gmail.com>
2016-04-22 00:19:12 -0700

    first pass at subsetting by first col and merging by pid

--- scripts/proc_study_brighten.R
+++ scripts/proc_study_brighten.R
@@ -4,11 +4,32 @@ rm(list = ls())
 
 library(aceR)
 
-setwd("~/Desktop/ACE Studies_Raw Data/Brighten")
+# this is data from dropbox NOT Google Drive
+
+setwd("~/Desktop/ACE data")
 
 # load & process dat
-dat = load_ace_bulk(recursive = FALSE)
+dat = load_ace_bulk(pattern = "BRT", recursive = FALSE)
 proc = proc_by_module(dat, TRUE)
+
+all_tasks = data.frame()
+module_names = names(proc)
+for (i in 1:seq(length(proc))) {
+  module = proc[[i]]
+  module = module[order(as.character(module$bid)), ]
+  module_name = module_names[i]
+  first_block = subset_first_block(module)
+  col_names = names(first_block)
+  names(first_block) = sapply(col_names, function(x) {
+    if (grepl("pid", x)) {
+      new_name = x
+    } else {
+      new_name = paste(module_name, x, sep = "_")
+    }
+    return (new_name)
+  })
+  all_tasks = plyr::join(all_tasks, first_block, by = "pid")
+}
 
 setwd("~/Desktop/brighten_test")
 export_csv(proc)
