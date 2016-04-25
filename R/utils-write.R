4da8dfc18863a2fda33397c35b50a737c71670c8
Jose Gallegos <josegallegos07@gmail.com>
2016-04-24 17:55:48 -0700

    support writing into subdirectories

--- R/utils-write.R
+++ R/utils-write.R
@@ -7,13 +7,19 @@
 #' @param dat a list of data frames to save
 #' @param path the named release directory
 
-export_csv <- function (dat, path = ".") {
+export_csv <- function (dat, path = ".", showWarnings = FALSE) {
   names = names(dat)
   for (i in names) {
     sub = as.data.frame(dat[i])
     strip = paste(i, ".", sep = "")
     names(sub) = sapply(names(sub), function (x) gsub(strip, "", x))
     file = paste(i, ".csv", sep = "")
+    if (path != ".") {
+      file = paste(path, file, sep = "/")
+      if (!dir.exists(path)) {
+        dir.create(file.path(path), showWarnings = showWarnings)
+      }
+    }
     print(paste("saving...", file))
     write.csv(sub, file, na = "")
   }
