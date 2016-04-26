6b6693493d6c401ea3adbce1477419c16ddf1fa6
Jose Gallegos <josegallegos07@gmail.com>
2016-04-25 23:13:34 -0700

    check-in script for loading processed data & adding demographics

---
+++ scripts/proc_all_demographs.R
@@ -0,0 +1,38 @@
+# loads processed files, adds demographics
+
+rm(list = ls())
+
+setwd("~/Desktop")
+
+library(aceR)
+
+# load demographics data
+demographics_file = "All Participant Demographics.xlsx"
+demographics = openxlsx::read.xlsx(demographics_file, sheet = 1, colNames = TRUE)
+demographics$pid = demographics[, 1]
+names(demographics) = tolower(gsub("[.]", "_", names(demographics)))
+
+# load & process data by module
+all_data_path = "ace_process"
+out_path = "ace_process_demographs"
+
+# make ouput directory
+if (!dir.exists(out_path)) {
+  dir.create(file.path(out_path), showWarnings = FALSE)
+}
+
+all_files = list.files(path = all_data_path, recursive = TRUE)
+mods = unique(basename(all_files))
+for (module in mods) {
+  # load data
+  mod_dat = load_files(path = all_data_path, pattern = module, recursive = TRUE)
+  # add demographic data
+  mod_demo = merge(mod_dat, demographics, by = "pid")
+  mod_demo = mod_demo[order(as.character(mod_demo$pid)), ]
+  row.names(mod_demo) = NULL
+  
+  # putut
+  mod_file_name = paste(out_path, module, sep ="/")
+  write.csv(mod_demo, mod_file_name)
+  
+}
