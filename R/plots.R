2f6b4607c5f0e570c0b5c4d2dd8209f6910a668e
Jose Gallegos <josegallegos07@gmail.com>
2016-04-26 21:29:11 -0700

    make_box_plot is wrapper function around ggplot2::ggplot

---
+++ R/plots.R
@@ -0,0 +1,22 @@
+
+#' @export
+#' @inheritParams base::list.files
+
+make_box_plot <- function(df, x, y, title, xlab, ylab, ...) {
+  if (missing(title)) {
+    title = paste(y, "by", x, sep = " ")
+  }
+  if (missing(xlab)) {
+    xlab = x
+  }
+  if (missing(ylab)) {
+    ylab = y
+  }
+  boxplot = ggplot2::ggplot(df, 
+    ggplot2::aes(x = df[, x], y = df[, y])) + 
+    ggplot2::geom_boxplot() + 
+    ggplot2::ggtitle(title) + 
+    ggplot2::xlab(xlab) + 
+    ggplot2::ylab(ylab)
+  return (boxplot)
+}
