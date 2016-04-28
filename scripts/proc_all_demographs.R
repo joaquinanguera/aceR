bdc4e27eb1028b4acf17523a461a3bb5e5ff3480
Jose Gallegos <josegallegos07@gmail.com>
2016-04-27 19:05:11 -0700

    spit out blox plots

--- scripts/proc_all_demographs.R
+++ scripts/proc_all_demographs.R
@@ -8,6 +8,11 @@ setwd(BASE)
 
 library(aceR)
 
+# define for plots
+factors = c("study_name", "age_group", "cohort_group")
+variable_patterns = c("rt_", "acc_", "rw_")
+variable_skip = c("cohort", "count", "length")
+
 # load demographics data
 demographics_file = "All Participant Demographics.xlsx"
 demographics = load_ace_demographics(demographics_file)
@@ -16,16 +21,19 @@ demographics = load_ace_demographics(demographics_file)
 all_data_path = "ace_process"
 out_path = "ace_process_demographs"
 out_path_first_block = paste(out_path, FIRST_BLOCK, sep = "/")
+out_path_first_block_plots = paste(out_path_first_block, "plots", sep = "/")
 
 # make ouput directories
 make_directory(out_path)
 make_directory(out_path_first_block)
+make_directory(paste(out_path_first_block_plots, "plots", sep = "/"))
 
 all_files = list.files(path = all_data_path, recursive = TRUE)
 mods = unique(basename(all_files))
 for (module in mods) {
   # load data
   mod_dat = load_files(path = all_data_path, pattern = module, recursive = TRUE)
+  mod_name = gsub(".csv", "", module)
   # add demographic data
   mod_demo = merge(mod_dat, demographics, by = "pid")
   mod_demo = mod_demo[order(as.character(mod_demo$pid)), ]
@@ -39,6 +47,15 @@ for (module in mods) {
   mod_demo_first_block = aceR:::subset_first_block(mod_demo)
   mod_demo_first_block_file_name = paste(out_path, "first_block", module, sep = "/")
   write.csv(mod_demo_first_block, mod_demo_first_block_file_name, na = "")
-
   
+  # make box plots
+  mod_demo_first_block_columns = names(mod_demo_first_block)
+  mod_demo_first_block_variables = aceR:::multi_filter_vec(mod_demo_first_block_columns, variable_patterns)
+  mod_demo_first_block_variables = aceR:::multi_filter_out_vec(mod_demo_first_block_variables, variable_skip)
+  for (group in factors) {
+    title_prefix = mod_name
+    file_prefix = paste(mod_name, group, sep = "_")
+    make_box_plot_bulk(mod_demo_first_block, group, mod_demo_first_block_variables, title_prefix, file_prefix, out_path_first_block_plots)
+  }
+
 }
