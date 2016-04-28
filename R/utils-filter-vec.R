c428e31a17ea2b9c45d98d69cfb116b0cc195ba0
Jose Gallegos <josegallegos07@gmail.com>
2016-04-27 18:59:16 -0700

    ad multi_filter_vec and multi_filter_out_vec methods

--- R/utils-filter-vec.R
+++ R/utils-filter-vec.R
@@ -9,9 +9,30 @@ filter_vec <- function(vec = c(), pattern = NULL) {
 
 #' @keywords internal
 
+multi_filter_vec <- function(vec = c(), patterns = c()) {
+  out = c()
+  for (i in 1:length(patterns)) {
+    filtered = filter_vec(vec, patterns[i])
+    out = c(out, filtered)
+  }
+  return (out)
+}
+
+#' @keywords internal
+
 filter_out_vec <- function(vec = c(), pattern = NULL) {
   match = sapply(vec, function(x) return (!grepl(pattern, x)))
   return (vec[which(match)])
+}
+
+#' @keywords internal
+
+multi_filter_out_vec <- function(vec = c(), patterns = c()) {
+  out = vec
+  for (i in 1:length(patterns)) {
+    out = filter_out_vec(out, patterns[i])
+  }
+  return (out)
 }
 
 #' @keywords internal
