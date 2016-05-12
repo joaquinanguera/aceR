c066d0afc7d47e2a756ff41031871f46763be2f8
Jose Gallegos <josegallegos07@gmail.com>
2016-05-11 21:06:28 -0700

    no plots for now

--- scripts/proc_all_demographs.R
+++ scripts/proc_all_demographs.R
@@ -26,7 +26,7 @@ out_path_first_block_plots = paste(out_path_first_block, "plots", sep = "/")
 # make ouput directories
 make_directory(out_path)
 make_directory(out_path_first_block)
-make_directory(out_path_first_block_plots)
+# make_directory(out_path_first_block_plots)
 
 all_files = list.files(path = all_data_path, recursive = TRUE)
 mods = unique(basename(all_files))
@@ -36,9 +36,12 @@ for (module in mods) {
   mod_dat = load_files(path = all_data_path, pattern = module, recursive = TRUE)
   mod_name = gsub(".csv", "", module)
   # add demographic data
-  mod_demo = merge(mod_dat, demographics, by = "pid")
+  mod_demo = merge(mod_dat, demographics, by = "pid", all = FALSE)
   mod_demo = mod_demo[order(as.character(mod_demo$pid)), ]
   row.names(mod_demo) = NULL
+  
+  # remove rows with out study_name
+  mod_demo = mod_demo[!is.na(mod_demo$study_name),]
   
   # output by tasks - nothing subsetted here
   mod_file_name = paste(out_path, module, sep ="/")
@@ -49,17 +52,17 @@ for (module in mods) {
   mod_demo_first_block_file_name = paste(out_path, "first_block", module, sep = "/")
   write.csv(mod_demo_first_block, mod_demo_first_block_file_name, na = "")
   
-  # make box plots
-  mod_demo_first_block_columns = names(mod_demo_first_block)
-  mod_demo_first_block_variables = aceR:::multi_filter_vec(mod_demo_first_block_columns, variable_patterns)
-  mod_demo_first_block_variables = aceR:::multi_filter_out_vec(mod_demo_first_block_variables, variable_skip)
-  for (group in factors) {
-    title_prefix = mod_name
-    file_prefix = paste(mod_name, group, sep = "_")
-    box_plots = make_box_plot_bulk(mod_demo_first_block, group, mod_demo_first_block_variables, title_prefix, file_prefix, out_path_first_block_plots)
-    for (boxplot in box_plots) {
-      plot(boxplot)
-    }
-  }
+  # # make box plots
+  # mod_demo_first_block_columns = names(mod_demo_first_block)
+  # mod_demo_first_block_variables = aceR:::multi_filter_vec(mod_demo_first_block_columns, variable_patterns)
+  # mod_demo_first_block_variables = aceR:::multi_filter_out_vec(mod_demo_first_block_variables, variable_skip)
+  # for (group in factors) {
+  #   title_prefix = mod_name
+  #   file_prefix = paste(mod_name, group, sep = "_")
+  #   box_plots = make_box_plot_bulk(mod_demo_first_block, group, mod_demo_first_block_variables, title_prefix, file_prefix, out_path_first_block_plots)
+  #   for (boxplot in box_plots) {
+  #     plot(boxplot)
+  #   }
+  # }
 
 }
