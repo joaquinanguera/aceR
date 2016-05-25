fcdb5150129a3772d87d5d47bea9ce2932524ffa
Jose Gallegos <josegallegos07@gmail.com>
2016-05-24 19:16:43 -0700

    cleanup

--- scripts/transform_woodcock.R
+++ scripts/transform_woodcock.R
@@ -8,106 +8,17 @@ library(plyr)
 
 # set directory 
 
-WORKING_DIR = "~/Desktop"
-setwd(WORKING_DIR)
+BASE_DIRECTORY = "~/Desktop/ace"
+DEMOGRAPHICS_FILE = paste(BASE_DIRECTORY, "All Participant Demographics.xlsx", sep = "/")
 
-# helper functions
-make_out_name = function(x) {
-  out = paste("wj_transform", tail(strsplit(x, " ")[[1]], n = 1), sep = "_")
-  out = tolower(gsub("xlsx", "csv", out))
-  return (out)
-}
+GRADE_FILE = paste(BASE_DIRECTORY, "woodcock", "School Pilot WJ Data GRADE.xlsx", sep = "/")
+AGE_FILE = paste(BASE_DIRECTORY, "woodcock", "School Pilot WJ Data AGE.xlsx", sep = "/")
 
-# load demographics data
-demographics_file = "All Participant Demographics.xlsx"
-demographics = load_ace_demographics(demographics_file)
+GRADE_OUT_FILE = paste(BASE_DIRECTORY, "woodcock", "wj_transform_grade.csv", sep = "/")
+AGE_OUT_FILE = paste(BASE_DIRECTORY, "woodcock", "wj_transform_age.csv", sep = "/")
 
-# loop
+grade_transformed = aceR:::transform_woodcock(GRADE_FILE)
+age_transformed = aceR:::transform_woodcock(AGE_FILE)
 
-files = c("School Pilot WJ Data GRADE.xlsx", "School Pilot WJ Data AGE.xlsx")
-
-for (file in files) {
-  file_name = file
-  out_name = make_out_name(file)
-  dat = openxlsx::read.xlsx(file_name, sheet = 1, colNames = FALSE)
-
-  # clean
-  header = dat[1,]
-  dat = dat[-1,] # get rid of header
-  dat = aceR:::remove_empty_rows(dat)
-
-  # init output
-  out = data.frame()
-
-  # group by student id & transform
-  sids = which(!is.na(dat[, 1]))
-  num_sids = length(sids)
-  grows = seq(1:num_sids)
-  gdat = list()
-  for (i in grows) {
-    
-    # group
-    first = sids[i]
-    last = ifelse(i == num_sids, nrow(dat), sids[i + 1] - 1) 
-    sdat = dat[first:last, ]
-    sid = sdat[1, 1]
-    names(sdat) = header
-    
-    # transform
-
-    prefix = tolower(sdat[, 3 ])
-    prefix = aceR:::to_title_case(prefix)
-    prefix = aceR:::replace_spaces(prefix, "")
-    
-    wood_dat = sdat[, 3:length(sdat)]
-    wood_dat[, 1] = prefix
-    id = names(wood_dat)[1]
-    wood_melt = reshape::melt(wood_dat, id = id)
-    
-    clust_prefix = unique(wood_melt[, 1])
-    clust_suffix = unique(wood_melt[, 2])
-    clust_seq = seq(clust_prefix)
-    clust_num = length(clust_suffix)
-    corrected_order = sapply(clust_seq, function(x) {
-      cname = sapply(clust_suffix, function(y) paste(clust_prefix[x], y, sep = "_"))
-      return (cname)
-    }) 
-    corrected_order = as.vector(corrected_order)
-    
-    user_dat = data.frame(
-      val = wood_melt$value
-    )
-    user_dat_t = data.frame(t(user_dat))
-    names(user_dat_t) = paste(wood_melt[, 1], wood_melt[, 2], sep = "_")
-    
-    # merge
-
-    wood = data.frame(
-      sid = sdat[1, 1],
-      date = sdat[1, 2]
-    ) 
-    
-    wood = merge(wood, user_dat_t[corrected_order])
-    out = plyr::rbind.fill(out, wood)
-    
-    # sort
-    
-    out$sid = gsub(" ", "", out$sid)
-    out = out[order(as.character(out$sid)), ] 
-    
-    # remove unneeded columns
-    
-    good_column_names = aceR:::filter_out_vec(names(out), "_to")
-    out = out[good_column_names]
-    
-    # add demographics data
-    
-    out$pid = paste("ADMIN-UCSF", out$sid, sep = "-")
-    out$pid = gsub("SP", "", out$pid)
-    woodcock = merge(out, demographics, by = "pid")
-    
-  }
-  # export
-  write.csv(woodcock, out_name)
-}
-
+write.csv(grade_transformed, GRADE_OUT_FILE)
+write.csv(age_transformed, AGE_OUT_FILE)
