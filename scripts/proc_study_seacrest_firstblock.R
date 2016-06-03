5c2d23886a14d4b9ed35ddade958d5190fa7f3b2
Jose Gallegos <josegallegos07@gmail.com>
2016-06-03 02:32:15 -0700

    ids have 3 characters

--- scripts/proc_study_seacrest_firstblock.R
+++ scripts/proc_study_seacrest_firstblock.R
@@ -50,6 +50,11 @@ standardize_pid = function(x) {
   standardized = sapply(x, function(y) { 
     id_split = stringr::str_split(y, "-")[[1]]
     id = id_split[3]
+    if (nchar(id) == 1) {
+      id = paste0("00", id)
+    } else if (nchar(id) == 2) {
+      id = paste0("0", id)
+    }
     return (paste0("ADMIN-UCSF-", gsub("b", "", id)))
   })
   return (as.vector(standardized))
