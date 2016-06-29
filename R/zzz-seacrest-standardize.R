4e3dac0cd56c80592aef35db3d1ea564819f1a29
Alan Duanmu <alan.duanmu@gmail.com>
2016-06-29 14:44:46 -0700

    Sometimes, id is missing

--- R/zzz-seacrest-standardize.R
+++ R/zzz-seacrest-standardize.R
@@ -10,10 +10,12 @@ standardize_seacrest_pid = function(x) {
   standardized = sapply(x, function(y) { 
     id_split = stringr::str_split(y, "-")[[1]]
     id = id_split[3]
-    if (nchar(id) == 1) {
-      id = paste0("00", id)
-    } else if (nchar(id) == 2) {
-      id = paste0("0", id)
+    if (!is.na(nchar(id))) {
+      if (nchar(id) == 1) {
+        id = paste0("00", id)
+      } else if (nchar(id) == 2) {
+        id = paste0("0", id)
+      }
     }
     return (paste0("ADMIN-UCSF-", gsub("b", "", id)))
   })
