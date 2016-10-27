5b5de3b54e0356bc611506f0436587d304435c60
monicathieu <mthieu@stanford.edu>
2016-10-27 13:57:47 -0700

    finalized: do cleaning funcs inside for loop for speed

--- R/utils-subset-df.R
+++ R/utils-subset-df.R
@@ -8,12 +8,10 @@ subset_by_col <- function(df, col) {
     group = unique_groups[i]
     match = which(df[col] == group)
     out[[i]] = df[match, ]
+    # do these inside for loop on df input, not list input, for efficiency
     out[[i]] = replace_nas(out[[i]], "")
     out[[i]] = remove_empty_cols(out[[i]])
   }
   names(out) = unique_groups
-  # TODO: improve efficiency of replace_nas, this is driving speed decrement of entire function
-  # out = replace_nas(out, "")
-  # out = remove_empty_cols(out)
   return (out)
 }
