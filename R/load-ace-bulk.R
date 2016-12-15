fd3fd6470642c9c5731ba375989bca157c041e13
monicathieu <mthieu@stanford.edu>
2016-12-14 19:50:41 -0800

    improving speed for large emailed datasets

--- R/load-ace-bulk.R
+++ R/load-ace-bulk.R
@@ -24,17 +24,55 @@ load_ace_bulk <- function(path = ".", verbose = TRUE, recursive = TRUE, exclude 
   if (length(files) == 0) {
     stop("no matching files", call. = TRUE)
   }
-  out = data.frame()
-  for (file in files) {
-    if (verbose) {
-      print(file)
+  # out =
+  if (length(files) > 50) {
+    out = list()
+    module_out = data.frame()
+    temp_out = data.frame()
+    sorted_files = sort_files_by_module(files) # files LISTED by module type
+    for (i in 1:length(sorted_files)) {
+      for (j in 1:length(sorted_files[[i]])) {
+        file = sorted_files[[i]][j]
+        if (verbose) {
+          print(file)
+        }
+        if (path != ".") {
+          file = paste(path, file, sep = "/")
+        }
+        dat = load_ace_file(file)
+        temp_out = plyr::rbind.fill(temp_out, dat)
+        if (i %% 50 == 0 | i == length(files)) {
+          module_out = plyr::rbind.fill(module_out, temp_out)
+          temp_out = data.frame()
+        }
+      }
+      out[[i]] = module_out
     }
-    if (path != ".") {
-      file = paste(path, file, sep = "/")
+    out = rbind.fill(out) # again, trying to save rbind.filling for the end in the hella emailed files case
+  } else {
+    out = data.frame()
+    for (i in 1:length(files)) {
+      file = files[i]
+      if (verbose) {
+        print(file)
+      }
+      if (path != ".") {
+        file = paste(path, file, sep = "/")
+      }
+      dat = load_ace_file(file)
+      out = plyr::rbind.fill(out, dat)
     }
-    dat = load_ace_file(file)
-    out = plyr::rbind.fill(out, dat)
   }
   out = replace_nas(out, "")
   return(out)
+}
+
+#' @keywords internal
+
+sort_files_by_module <- function(files, modules = c(BOXED, BRT, FLANKER, SAAT, SPATIAL_SPAN, STROOP, TASK_SWITCH, TNT)) {
+  sorted_files = list()
+  for (module in modules) {
+    sorted_files[[module]] = files[grepl(module, files, ignore.case = T)]
+  }
+  return (sorted_files)
 }
