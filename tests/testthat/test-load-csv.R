d198986e7fb50c2a93625a9a16365666c8d72d5f
monicathieu <mthieu@stanford.edu>
2018-01-02 16:54:08 -0600

    adding pulvinar test

--- tests/testthat/test-load-csv.R
+++ tests/testthat/test-load-csv.R
@@ -3,7 +3,10 @@ context("load raw csv data")
 sample_bad = paste(aceR_sample_data_path(), "bad-data.csv", sep = "/")
 sample_valid_but_bad_because_empty = paste(aceR_sample_data_path(), "bad-data-2.csv", sep = "/")
 sample_boxed = paste(aceR_sample_data_path(), "sample-boxed.csv", sep = "/")
+sample_brt_pulvinar = paste(aceR_sample_data_path(), "sample-brt-pulvinar.csv", sep = "/")
+
 
 expect_warning(load_ace_file(sample_bad))
 expect_warning(load_ace_file(sample_valid_but_bad_because_empty))
 expect_gt(nrow(load_ace_file(sample_boxed)), 0)
+expect_gt(nrow(load_ace_file(sample_brt_pulvinar)), 0)
