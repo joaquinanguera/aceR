644671f78151e77fb082256f98ee23fdfac26f41
monicathieu <mthieu@stanford.edu>
2018-02-24 15:23:44 -0500

    doesn't rename "fractions" module in module column

--- R/load-sea-constants-modules.R
+++ R/load-sea-constants-modules.R
@@ -20,7 +20,6 @@ label_sea_module_conditions <- function (dat) {
                                         "Fractions Lvl 2" = "lvl_2",
                                         "Fractions Lvl 3" = "lvl_3",
                                         .default = "")
-  dat[[COL_MODULE]] <- ifelse(startsWith(dat[[COL_MODULE]], "Fractions"), "Fractions", dat[[COL_MODULE]])
   dat[[COL_MODULE]] <- ifelse(startsWith(dat[[COL_MODULE]], "Reading Fluency"), "Reading Fluency", dat[[COL_MODULE]])
   return (dat)
 }
