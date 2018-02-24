98ac588f8d2ec9b9194062d1be5c81a569081b9a
monicathieu <mthieu@stanford.edu>
2018-02-24 15:03:48 -0500

    renaming to accommodate sea version

---
+++ R/load-ace-constant-columns.R
@@ -0,0 +1,104 @@
+
+#' ACE column name constants
+#'
+#' @keywords internal
+#' @name ace_header
+NULL
+
+#' @name ace_header
+COL_FILE = "file"
+
+#' @name ace_header
+COL_TIME = "time"
+
+#' @name ace_header
+COL_RT = "rt"
+
+#' @name ace_header
+COL_CORRECT_BUTTON = "correct_button"
+
+#' @name ace_header
+COL_CORRECT_RESPONSE = "correct_response"
+
+#' @name ace_header
+COL_RW = "rw"
+
+#' @name ace_header
+COL_LATE_RESPONSE = "late_response"
+
+#' @name ace_header
+COL_CONDITION = "condition"
+
+#' @name ace_header
+COL_STUDY_COND = "study_condition"
+
+#' @name ace_header
+COL_PID = "pid"
+
+#' @name ace_header
+COL_BID = "bid"
+
+#' @name ace_header
+COL_NAME = "name"
+
+#' @name ace_header
+COL_AGE = "age"
+
+#' @name ace_header
+COL_GRADE = "grade"
+
+#' @name ace_header
+COL_GENDER = "gender"
+
+#' @name ace_header
+COL_HANDEDNESS = "handedness"
+
+#' @name ace_header
+COL_SUB_ID = "subid"
+
+#' @name ace_header
+COL_TRIAL_TYPE = "trial_type"
+
+#' @name ace_header
+COL_BLOCK_HALF = "half"
+
+#' @name ace_header
+
+standardize_ace_column_names <- function(df) {
+  new = names(df)
+  new[new == "response_time"] = COL_RT
+  new[new == "response_window"] = COL_RW
+  new[new == "participant_id"] = COL_PID
+  new[new == "user_id"] = COL_PID
+  new[new == "user_name"] = COL_NAME
+  new[new == "user_age"] = COL_AGE
+  new[new == "user_grade"] = COL_GRADE
+  new[new == "user_gender"] = COL_GENDER
+  new[new == "age1"] = COL_GENDER
+  new[new == "user_age1"] = COL_GENDER
+  new[new == "user_handedness"] = COL_HANDEDNESS
+  new[new == "time_gameplayed_utc"] = COL_TIME
+  new[new == "time_sent_utc"] = "timesent_utc"
+  new[new == "id"] = COL_SUB_ID
+  new[new == "details"] = COL_CONDITION
+  new[new == "task_switch_state"] = "taskswitch_state" # for cross compatibility b/w emailed and pulvinar
+  names(df) = new
+  return (df)
+}
+
+#' @name ace_header
+#' @importFrom dplyr recode
+
+standardize_ace_values <- function(df) {
+  cols = names(df)
+  if (COL_CORRECT_BUTTON %in% cols) {
+    df[, COL_CORRECT_BUTTON] = dplyr::recode(df[, COL_CORRECT_BUTTON], `0` = "incorrect", `1` = "correct")
+  }
+  if (COL_CORRECT_RESPONSE %in% cols) {
+    df[, COL_CORRECT_RESPONSE] = dplyr::recode(df[, COL_CORRECT_RESPONSE], `0` = "incorrect", `1` = "correct")
+  }
+  if (COL_LATE_RESPONSE %in% cols) {
+    df[, COL_LATE_RESPONSE] = dplyr::recode(df[, COL_LATE_RESPONSE], `0` = "early", `1` = "late")
+  }
+  return (df)  
+}
