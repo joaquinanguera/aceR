5d6740059b959ece496d8df2b6c752a3d95dab5d
monicathieu <mthieu@stanford.edu>
2018-05-19 15:49:48 -0400

    deleting old versions of functions

--- R/module-constants.R
+++ R/module-constants.R
@@ -1,41 +1,10 @@
 
 #' @keywords internal
-
-proc_generic_module_old <- function(df, col_acc, col_condition, by_factor = TRUE) {
-  # overall & broken-down by condition
-  rt = proc_by_condition(df, COL_RT, col_condition, FUN = ace_descriptive_statistics)
-  rw = proc_by_condition(df, COL_RW, col_condition, FUN = ace_descriptive_statistics)
-  acc = proc_by_condition(df, col_acc, col_condition, FUN = ace_descriptive_statistics)
-  # RT broken-down by condition & factor  
-  rt_acc = proc_standard(df, COL_RT, col_condition, factor = col_acc, FUN = ace_descriptive_statistics_by_group)
-  # RW & RT by block half
-  rt_block_half = proc_standard(df, COL_RT, NULL, factor = COL_BLOCK_HALF, FUN = ace_descriptive_statistics_by_group)
-  rw_block_half = proc_standard(df, COL_RW, NULL, factor = COL_BLOCK_HALF, FUN = ace_descriptive_statistics_by_group)
-  # turns calculations
-  if (by_factor) {
-    # turns calculation by condition
-    turns = proc_standard(df, COL_RW, col_condition, factor = col_acc, FUN = ace_average_turns)
-    # detection rates by condition
-    detection = proc_standard(df, COL_RW, col_condition, factor = COL_RT, FUN = ace_detection_rate)
-  } else {
-    # overall turns calculation
-    turns = proc_standard(df, COL_RW, col_condition = NULL, factor = col_acc, FUN = ace_average_turns, y = c(COL_BID))
-    # overall detection rates
-    detection = proc_standard(df, COL_RW, col_condition = NULL, factor = COL_RT, FUN = ace_detection_rate, y = c(COL_BID))
-  }
-  # merge
-  analy = list(rt, acc, rw, rt_acc, rt_block_half, rw_block_half, turns, detection)
-  merged = multi_merge(analy, by = COL_BID)
-  return (merged)
-}
-
-
-#' @keywords internal
+#' @param df data for one module
+#' @param col_acc column for accuracy, as string
+#' @param col_condition column containing "condition" or trial type, as string
 
 proc_generic_module <- function(df, col_acc, col_condition, FUN = ace_descriptive_statistics_dplyr) {
-  # df: the data
-  # col_acc: name of col containing accuracy
-  # col_condition: name of col containing condition/trial type
   
   # overall & broken-down by condition
   # RT broken-down by condition & accuracy  
@@ -65,26 +34,6 @@ proc_generic_module <- function(df, col_acc, col_condition, FUN = ace_descriptiv
   
   merged = multi_merge(analy, by = COL_BID)
   return (merged)
-}
-
-#' @keywords internal
-
-proc_by_condition_old <- function(df, variable, col_condition, FUN) {
-  overall = apply_stats(
-    x = df, 
-    y = c(COL_BID), 
-    col = variable, 
-    FUN = FUN, 
-    suffix = "overall")
-  by_condition = apply_stats(
-    x = df, 
-    y = c(COL_BID, col_condition), 
-    col = variable, 
-    FUN = FUN)
-  by_condition_transform = stats::reshape(by_condition, timevar = col_condition, idvar = COL_BID, direction = "wide") 
-  proc = multi_merge(list(overall, by_condition_transform), by = COL_BID)
-  names(proc) = tolower(names(proc))
-  return(proc)
 }
 
 #' @keywords internal
