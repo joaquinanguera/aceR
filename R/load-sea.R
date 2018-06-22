6af625461efa50fca8555ae6169511b1e3849bcf
monicathieu <mthieu@stanford.edu>
2018-06-21 20:51:10 -0700

    tidyverse-ing

--- R/load-sea.R
+++ R/load-sea.R
@@ -4,11 +4,11 @@
 #' Reads, parses, and converts an SEA csv into an R \code{\link{data.frame}}.
 #'
 #' @export
-#' @importFrom dplyr mutate_if
-#' @importFrom utils read.csv
+#' @importFrom dplyr case_when group_by if_else lag mutate mutate_if ungroup %>%
+#' @importFrom purrrr nest unnest map2
 #' @importFrom stringr str_trim
 #' @importFrom tidyr unite
-#' @importFrom rlang !! sym UQ
+#' @importFrom rlang !! parse_expr sym
 #' @param file The name of the file which the data is to be read from.
 #' @param verbose Print file name (as progress checker)? Defaults to \code{FALSE}.
 #' @return Returns the file's content as an R \code{\link{data.frame}}.
@@ -19,22 +19,26 @@ load_sea_file <- function (file, verbose = FALSE) {
   if (nrow(dat) == 0) {
     warning(paste0(file, " contains no data!"))
     return (data.frame())
-    }
+  }
   # standardize case convention in colnames
   names(dat) <- standardize_names(dat)
-  dat$file <- file
-  dat <- tidyr::unite(dat, time, current_date, current_year, current_time, sep = " ")
-  dat <- dplyr::mutate_if(dat, is.character, stringr::str_trim)
-  dat <- standardize_sea_column_names(dat)
-  dat <- label_sea_module_conditions(dat)
-  dat <- standardize_sea_module_names(dat)
+  
   dat <- dat %>%
-    dplyr::group_by(!! sym(UQ(COL_PID))) %>%
-    dplyr::mutate(bid = paste(.data[[COL_PID]], .data[[COL_TIME]][1]),
-                  correct_button = ifelse(tolower(.data[[COL_RESPONSE]]) == tolower(.data[[COL_CORRECT_RESPONSE]]),
-                                          "correct", "incorrect"),
-                  half = dplyr::recode(make_half_seq(n()), `1` = "first_half", `2` = "second_half")) %>%
-    dplyr::ungroup()
+    mutate(file = file) %>%
+    unite(time, current_date, current_year, current_time, sep = " ") %>%
+    mutate_if(is.character, stringr::str_trim) %>%
+    standardize_sea_column_names() %>%
+    label_sea_module_conditions() %>%
+    standardize_sea_module_names() %>%
+    group_by(!! sym(COL_PID)) %>%
+    mutate(bid = paste(.data[[COL_PID]], .data[[COL_TIME]][1]),
+           correct_button = ifelse(tolower(.data[[COL_RESPONSE]]) == tolower(.data[[COL_CORRECT_RESPONSE]]),
+                                   "correct", "incorrect"),
+           half = dplyr::recode(make_half_seq(n()), `1` = "first_half", `2` = "second_half")) %>%
+    group_by(!! sym(COL_MODULE)) %>%
+    nest() %>%
+    mutate(data = map2(data, module, ~append_info(.x, module = .y))) %>%
+    unnest()
   return (dat)
 }
 
@@ -43,6 +47,7 @@ load_sea_file <- function (file, verbose = FALSE) {
 #' one cell into two, creating too many columns for just a few rows
 #' 
 #' @importFrom stringr str_trim
+#' @importFrom utils read.table
 #' @keywords internal
 
 read_sea_csv <- function(file) {
@@ -51,18 +56,18 @@ read_sea_csv <- function(file) {
   
   if (all(unique(dat[, 31]) == "") | all(is.na(dat[, 31]))) return (dat)
   
-  names(dat) <- c(stringr::str_trim(dat[1, 1:30]), "junk", "junk2")
+  names(dat) <- c(str_trim(dat[1, 1:30]), "junk", "junk2")
   dat <- dat[2:nrow(dat), ]
   if (all(is.na(dat$junk2))) {
     bad_rows <- which(dat$junk != "")
     dat[bad_rows, "Question Text"] <- paste(dat[bad_rows, "Question Text"],
-                                             dat[bad_rows, "Question Type"], sep = ",")
+                                            dat[bad_rows, "Question Type"], sep = ",")
     # Moving these bad rows' data after the bad delimiter one column to the left
     dat[bad_rows, 6:30] <- dat[bad_rows, 7:31]
   } else {
     bad_rows1 <- which(dat$junk != "" & dat$junk2 == "")
     dat[bad_rows1, "Question Text"] <- paste(dat[bad_rows1, "Question Text"],
-                                            dat[bad_rows1, "Question Type"], sep = ",")
+                                             dat[bad_rows1, "Question Type"], sep = ",")
     # Moving these bad rows' data after the bad delimiter one column to the left
     dat[bad_rows1, 6:30] <- dat[bad_rows1, 7:31]
     
