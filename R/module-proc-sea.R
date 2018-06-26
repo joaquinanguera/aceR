d8af72e2da8e25332221dc03b6c0b41f0e65ddbb
monicathieu <mthieu@stanford.edu>
2018-06-26 12:21:51 -0700

    again fixing dependencies

--- R/module-proc-sea.R
+++ R/module-proc-sea.R
@@ -18,7 +18,7 @@ NULL
 #' @import dplyr
 #' @importFrom stats aggregate median na.omit qnorm sd time var
 #' @importFrom stringr str_replace
-#' @importFrom purrr map map2 reduce
+#' @importFrom purrr map map2 pmap reduce
 #' @importFrom tidyr nest
 #' @param df a \code{\link{data.frame}} containing formatted trialwise SEA data. 
 #'
