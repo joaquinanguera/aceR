8f6ef1391be1cd7336d75feeeb56e1db135503b9
monicathieu <mthieu@stanford.edu>
2018-06-27 21:32:22 -0700

    fixing cost-calc-ing modules to not duplicate .overall cols

--- R/module-modules-sea.R
+++ R/module-modules-sea.R
@@ -15,46 +15,39 @@ attempt_module <- function(df, module, verbose) {
 
 #' @keywords internal
 #' @name sea_procs
+#' @importFrom purrr map map2 reduce
 
 module_math_fluency <- function(df) {
-  gen_stay_switch = proc_generic_module(df, COL_CORRECT_BUTTON, COL_CONDITION, FUN = sea_descriptive_statistics)
-  gen_operation_type = proc_generic_module(df, COL_CORRECT_BUTTON, "operation_type", FUN = sea_descriptive_statistics)
-  gen_answer_size = proc_generic_module(df, COL_CORRECT_BUTTON, "answer_size", FUN = sea_descriptive_statistics)
+  out <- tibble(condition = c(COL_CONDITION, "operation_type", "answer_size"),
+                cost_args = list(c("\\.stay", "\\.switch", "\\.switch_cost"),
+                                 c("\\.addition", "\\.subtraction", "\\.operation_cost"),
+                                 c("\\.1", "\\.2", "\\.answer_size_cost"))) %>%
+    mutate(gen = map(condition, function(x) proc_generic_module(df, COL_CORRECT_BUTTON, x, FUN = sea_descriptive_statistics)),
+           cost = map2(gen, cost_args, ~multi_subtract(.x, .y[1], .y[2], .y[3])),
+           both = map2(gen, cost, ~dplyr::bind_cols(.x, .y)),
+           names = map(both, ~names(.x)))
   
-  cost_stay_switch = multi_subtract(gen_stay_switch, "\\.stay", "\\.switch", "\\.switch_cost")
-  cost_operation_type = multi_subtract(gen_operation_type, "\\.addition", "\\.subtraction", "\\.operation_cost")
-  cost_answer_size = multi_subtract(gen_answer_size, "\\.1", "\\.2", "\\.answer_size_cost")
-  
-  return (dplyr::bind_cols(gen_stay_switch,
-                           cost_stay_switch,
-                           gen_operation_type,
-                           cost_operation_type,
-                           gen_answer_size,
-                           cost_answer_size))
+  duplicate_cols <- unique(unlist(out$names)[duplicated(unlist(out$names))])
+  return (purrr::reduce(out$both, full_join, by = duplicate_cols))
 }
 
 #' @keywords internal
 #' @name sea_procs
+#' @importFrom purrr map map2 reduce
 
 module_math_recall <- function(df) {
-  gen_carry = proc_generic_module(df, COL_CORRECT_BUTTON, "carrying", FUN = sea_descriptive_statistics)
-  gen_orientation = proc_generic_module(df, COL_CORRECT_BUTTON, "math_recall_orientation", FUN = sea_descriptive_statistics)
-  gen_operation_type = proc_generic_module(df, COL_CORRECT_BUTTON, "operation_type", FUN = sea_descriptive_statistics)
-  gen_answer_size = proc_generic_module(df, COL_CORRECT_BUTTON, "answer_size", FUN = sea_descriptive_statistics)
+  out <- tibble(condition = c("carrying", "math_recall_orientation", "operation_type", "answer_size"),
+                cost_args = list(c("\\.no_carry", "\\.carry", "\\.carry_cost"),
+                                 c("\\.horizontal", "\\.vertical", "\\.orientation_cost"),
+                                 c("\\.addition", "\\.subtraction", "\\.operation_cost"),
+                                 c("\\.1", "\\.2", "\\.answer_size_cost"))) %>%
+    mutate(gen = map(condition, function(x) proc_generic_module(df, COL_CORRECT_BUTTON, x, FUN = sea_descriptive_statistics)),
+           cost = map2(gen, cost_args, ~multi_subtract(.x, .y[1], .y[2], .y[3])),
+           both = map2(gen, cost, ~dplyr::bind_cols(.x, .y)),
+           names = map(both, ~names(.x)))
   
-  cost_carry = multi_subtract(gen_carry, "\\.no_carry", "\\.carry", "\\.carry_cost")
-  cost_orientation = multi_subtract(gen_orientation, "\\.horizontal", "\\.vertical", "\\.orientation_cost")
-  cost_operation_type = multi_subtract(gen_operation_type, "\\.addition", "\\.subtraction", "\\.operation_cost")
-  cost_answer_size = multi_subtract(gen_answer_size, "\\.1", "\\.2", "\\.answer_size_cost")
-  
-  return (dplyr::bind_cols(gen_carry,
-                           cost_carry,
-                           gen_orientation,
-                           cost_orientation,
-                           gen_operation_type,
-                           cost_operation_type,
-                           gen_answer_size,
-                           cost_answer_size))
+  duplicate_cols <- unique(unlist(out$names)[duplicated(unlist(out$names))])
+  return (purrr::reduce(out$both, full_join, by = duplicate_cols))
 }
 
 #' @keywords internal
@@ -98,37 +91,46 @@ module_fractions_lvl_2 <- function(df) {
 
 #' @keywords internal
 #' @name sea_procs
+#' @importFrom purrr map map2 reduce
 
 module_fractions_lvl_3 <- function(df) {
   # by left vs right and by numerator larger but not crossed
-  gen = proc_generic_module(df, COL_CORRECT_BUTTON, COL_CONDITION, FUN = sea_descriptive_statistics)
-  gen_num_larger = proc_generic_module(df, COL_CORRECT_BUTTON, "num_larger", FUN = sea_descriptive_statistics)
-  cost = multi_subtract(gen, "\\.left", "\\.right", "\\.cost")
-  cost_num_larger = multi_subtract(gen, "\\.num_larger", "\\.denom_larger", "\\.num_larger_cost")
-  return (dplyr::bind_cols(gen,
-                           cost,
-                           gen_num_larger,
-                           cost_num_larger))
+  out <- tibble(condition = c(COL_CONDITION, "num_larger"),
+                cost_args = list(c("\\.left", "\\.right", "\\.cost"),
+                                 c("\\.num_larger", "\\.denom_larger", "\\.num_larger_cost"))) %>%
+    mutate(gen = map(condition, function(x) proc_generic_module(df, COL_CORRECT_BUTTON, x, FUN = sea_descriptive_statistics)),
+           cost = map2(gen, cost_args, ~multi_subtract(.x, .y[1], .y[2], .y[3])),
+           both = map2(gen, cost, ~dplyr::bind_cols(.x, .y)),
+           names = map(both, ~names(.x)))
+  
+  duplicate_cols <- unique(unlist(out$names)[duplicated(unlist(out$names))])
+  return (purrr::reduce(out$both, full_join, by = duplicate_cols))
 }
 
 #' @keywords internal
 #' @name sea_procs
+#' @importFrom purrr map map2 reduce
 
 module_arithmetic_verification <- function(df) {
+  
   gen = proc_generic_module(df, COL_CORRECT_BUTTON, "block_type", FUN = sea_descriptive_statistics)
-  gen_mixed = proc_generic_module(filter(df, block_type == "Mixed"), COL_CORRECT_BUTTON, COL_CONDITION, FUN = sea_descriptive_statistics)
-  gen_mixed_full = proc_generic_module(filter(df, block_type == "Mixed"), COL_CORRECT_BUTTON, "switch_by_operation_type", FUN = sea_descriptive_statistics)
+  gen_mixed = proc_generic_module(filter(df, block_type == "Mixed"), COL_CORRECT_BUTTON, COL_CONDITION, FUN = sea_descriptive_statistics) %>%
+    select(-ends_with(".overall"), -ends_with("_half"), -ends_with("correct"))
+  gen_mixed_full = proc_generic_module(filter(df, block_type == "Mixed"), COL_CORRECT_BUTTON, "switch_by_operation_type", FUN = sea_descriptive_statistics) %>%
+    select(-ends_with(".overall"), -ends_with("_half"), -ends_with("correct"))
+  
+  gens = purrr::reduce(list(gen, gen_mixed, gen_mixed_full), full_join, by = "bid")
+  
   cost = multi_subtract(gen, "\\.fixed", "\\.mixed", "\\.cost")
   cost_mixed = multi_subtract(gen_mixed, "\\.stay", "\\.switch", "\\.cost")
   cost_mixed_multiplication = multi_subtract(gen_mixed_full, "\\.stay_multiplication", "\\.switch_multiplication", "\\.cost_multiplication")
   cost_mixed_addition = multi_subtract(gen_mixed_full, "\\.stay_addition", "\\.switch_addition", "\\.cost_addition")
-  return (dplyr::bind_cols(gen,
-                           cost,
-                           gen_mixed,
-                           cost_mixed,
-                           gen_mixed_full,
-                           cost_mixed_multiplication,
-                           cost_mixed_addition))
+  
+  return (dplyr::bind_cols(list(gens,
+                                cost,
+                                cost_mixed,
+                                cost_mixed_multiplication,
+                                cost_mixed_addition)))
 }
 
 #' @keywords internal
