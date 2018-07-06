c7d5588aa650463fdfd928b4aad18988daf9d5e8
monicathieu <mthieu@stanford.edu>
2018-07-05 17:41:49 -0700

    moving sample_data call inside test function

--- tests/testthat/test-load-ace.R
+++ tests/testthat/test-load-ace.R
@@ -1,8 +1,8 @@
 context("load raw SEA csv data")
 
-sample_data = paste(aceR_sample_data_path(), "sample-sea.csv", sep = "/")
-
 test_that("SEA data loads properly", {
+  sample_data = paste(aceR_sample_data_path(), "sample-sea.csv", sep = "/")
+  
   expect_false(any(endsWith(load_sea_file(sample_data)[[COL_MODULE]], ".")))
   expect_is(load_sea_bulk(aceR_sample_data_path(), pattern = "sea", verbose = F), "data.frame")
 })
