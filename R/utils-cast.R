22554b118893d3ad6d2b6b4f1e03887c21b8c5ba
monicathieu <mthieu@stanford.edu>
2018-07-15 15:54:48 -0700

    should cease to throw warning if called on already numeric input

--- R/utils-cast.R
+++ R/utils-cast.R
@@ -2,6 +2,10 @@
 #' @keywords internal
 
 to_numeric <- function(x) {
-  vals = plyr::mapvalues(x, from = c("incorrect", "correct"), to = c(0, 1), warn_missing = FALSE)
-  return(as.numeric(as.character(vals)))
+  if (is.numeric(x)) {
+    return (x)
+  } else {
+    vals = plyr::mapvalues(x, from = c("incorrect", "correct"), to = c(0, 1), warn_missing = FALSE)
+    return (as.numeric(as.character(vals)))
+  }
 }
