26a7f736ed43a2cb373c360952fb6beaf313b18d
monicathieu <mthieu@stanford.edu>
2018-07-15 17:23:42 -0700

    now REALLY doesn't throw warnings; dplyr::recode is stricter about typing

--- R/utils-cast.R
+++ R/utils-cast.R
@@ -5,7 +5,13 @@ to_numeric <- function(x) {
   if (is.numeric(x)) {
     return (x)
   } else {
-    vals = plyr::mapvalues(x, from = c("incorrect", "correct"), to = c(0, 1), warn_missing = FALSE)
-    return (as.numeric(as.character(vals)))
+    # being more type-strict than plyr::mapvalues, this WILL return numeric every time
+    vals = dplyr::recode(x,
+                         correct = 1L,
+                         incorrect = 0L,
+                         no_response = NA_integer_,
+                         .default = NA_integer_,
+                         .missing = NA_integer_)
+    return (vals)
   }
 }
