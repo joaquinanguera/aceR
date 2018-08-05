41dc7eb6931bdf08d4ea2d69bdbf559806aeb4e3
monicathieu <mthieu@stanford.edu>
2018-08-05 11:50:14 -0400

    fixing issue where to_numeric would fail on actual numbers coded as char

--- R/utils-cast.R
+++ R/utils-cast.R
@@ -7,11 +7,11 @@ to_numeric <- function(x) {
   } else {
     # being more type-strict than plyr::mapvalues, this WILL return numeric every time
     vals = dplyr::recode(x,
-                         correct = 1L,
-                         incorrect = 0L,
-                         no_response = NA_integer_,
-                         .default = NA_integer_,
-                         .missing = NA_integer_)
+                         correct = 1,
+                         incorrect = 0,
+                         no_response = NA_real_,
+                         .default = as.numeric(x),
+                         .missing = NA_real_)
     return (vals)
   }
 }
