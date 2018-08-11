9607b9c8e44de514900d3da944bda3a268dc108c
monicathieu <mthieu@stanford.edu>
2018-08-11 12:39:49 -0400

    improving functionality for converting acc and rt cols

--- R/utils-cast.R
+++ R/utils-cast.R
@@ -1,17 +1,20 @@
 
+#' @importFrom magrittr %>%
 #' @keywords internal
 
 to_numeric <- function(x) {
   if (is.numeric(x)) {
     return (x)
-  } else {
+  } else if ("correct" %in% unique(x) | "incorrect" %in% unique(x)) { # if it's an accuracy column
     # being more type-strict than plyr::mapvalues, this WILL return numeric every time
-    vals = dplyr::recode(x,
-                         correct = 1,
-                         incorrect = 0,
-                         no_response = NA_real_,
-                         .default = as.numeric(x),
-                         .missing = NA_real_)
+    vals = x %>%
+      dplyr::na_if("") %>%
+      dplyr::recode(correct = 1,
+                    incorrect = 0,
+                    no_response = NA_real_,
+                    .missing = NA_real_)
     return (vals)
+  } else { # if it's an RT column
+    return(as.numeric(x))
   }
 }
