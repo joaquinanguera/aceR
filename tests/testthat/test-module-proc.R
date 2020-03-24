98d1368373d4334bb24f23c7a0539dec519bfe86
monicathieu <mkt2131@columbia.edu>
2020-03-24 16:54:38 -0500

    add test for BRT

--- tests/testthat/test-module-proc.R
+++ tests/testthat/test-module-proc.R
@@ -3,11 +3,16 @@ context("process summary stats on loaded data")
 raw_explorer <- load_ace_bulk(aceR_sample_data_path("explorer"), app_type = "explorer", verbose = F)
 
 test_that("ACE Ishihara works", {
-  expect_named(module_ishihara(raw_explorer$data[[ISHIHARA]]),
+  expect_named(attempt_module(raw_explorer$data[[ISHIHARA]], ISHIHARA, verbose = FALSE),
                expected = c(COL_BID, "colorblind"))
 })
 
+test_that("ACE BRT works", {
+  expect_gt(ncol(attempt_module(raw_explorer$data[[BRT]], BRT, verbose = FALSE)), 1)
+  expect_warning(attempt_module(raw_explorer$data[[BRT]], BRT, verbose = FALSE),
+                 regexp = "No handedness data found")
+})
+
 test_that("ACE Explorer data bulk processes properly", {
-  
-  expect_gt(nrow(proc_by_module(raw_explorer, app_type = "explorer", verbose = F)), 0)
+  expect_gt(nrow(proc_by_module(raw_explorer, app_type = "explorer", output = "long", verbose = F)), 0)
 })
