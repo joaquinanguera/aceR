70651c08fc2c3efc8d60c67fc4b7601eb51f097a
monicathieu <mkt2131@columbia.edu>
2020-04-04 00:55:26 -0500

    add some proper t3 email data to testing

--- tests/testthat/test-load-bulk.R
+++ tests/testthat/test-load-bulk.R
@@ -2,6 +2,12 @@ context("bulk load multiple raw files")
 
 test_that("ACE data loads properly", {
   expect_is(load_ace_bulk(aceR_sample_data_path("explorer"), app_type = "explorer", verbose = F), "data.frame")
+  expect_is(load_ace_bulk(aceR_sample_data_path("email"),
+                          exclude = "bad-data",
+                          pattern = ".csv",
+                          app_type = "email",
+                          verbose = F),
+            "data.frame")
 })
 
 test_that("SEA data loads properly", {
--- tests/testthat/test-module-proc.R
+++ tests/testthat/test-module-proc.R
@@ -2,6 +2,12 @@ context("process summary stats on loaded data")
 
 raw_explorer <- load_ace_bulk(aceR_sample_data_path("explorer"), app_type = "explorer", verbose = F)
 
+raw_email <- load_ace_bulk(aceR_sample_data_path("email"),
+                           exclude = "bad-data",
+                           pattern = ".csv",
+                           app_type = "email",
+                           verbose = F)
+
 test_that("ACE Ishihara works", {
   expect_named(attempt_module(raw_explorer$data[[ISHIHARA]], ISHIHARA, verbose = FALSE),
                expected = c(COL_BID, "colorblind"))
@@ -13,6 +19,10 @@ test_that("ACE BRT works", {
                  regexp = "No handedness data found")
 })
 
-test_that("ACE Explorer data bulk processes properly", {
+test_that("ACE Classroom email data bulk processes properly", {
   expect_gt(nrow(proc_by_module(raw_explorer, app_type = "explorer", output = "long", verbose = F)), 0)
+})
+
+test_that("ACE Explorer data bulk processes properly", {
+  expect_gt(nrow(proc_by_module(raw_email, app_type = "classroom", output = "long", verbose = F)), 0)
 })
