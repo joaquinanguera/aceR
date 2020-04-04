54267258bee7e0f2e764fb312fa2c172d081a54e
monicathieu <mkt2131@columbia.edu>
2020-04-03 23:16:05 -0500

    adding proper quasiquoting to global vars

--- R/load-ace-bulk.R
+++ R/load-ace-bulk.R
@@ -54,17 +54,17 @@ load_ace_bulk <- function(path = ".",
   
   out <- out %>%
     filter(map(data, ~nrow(.)) > 0) %>% # if extraction failed, data will have 0 rows and other commands on data will fail
-    mutate(data = map(data, ~nest(as_tibble(.x), data = -c(bid, module, file)))) %>%
-    select(-file) %>%
+    mutate(data = map(data, ~nest(as_tibble(.x), data = -c(!!COL_BID, !!COL_MODULE, !!COL_FILE)))) %>%
+    select(-!!COL_FILE) %>%
     unnest(data) %>%
     # new de-duplication strategy: dplyr::distinct() files by bid & module should work (NOT by file)
     # NOTE: old de-duplication was done only on emailed data,
     # but this should behave properly for both emailed and database data
-    distinct(bid, module, .keep_all = TRUE) %>%
-    nest(data = -module) %>%
+    distinct(!!Q_COL_BID, !!Q_COL_MODULE, .keep_all = TRUE) %>%
+    nest(data = -!!COL_MODULE) %>%
     mutate(data = map(data, ~unnest(.x, data)),
            data = map(data, ~remove_empty_cols(.)),
-           data = rlang::set_names(data, module))
+           data = rlang::set_names(data, !!Q_COL_MODULE))
   
   if (app_type == "email") {
     # Set demos to the side to simulate ACE Explorer
