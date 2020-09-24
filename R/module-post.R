21392dcd004be66afc6d01f78cdf216f5bdd4e4b
monicathieu <mkt2131@columbia.edu>
2020-09-24 10:23:37 -0500

    SAAT sustained impulsive now separated in post cleans

--- R/module-post.R
+++ R/module-post.R
@@ -79,7 +79,7 @@ post_clean_chance <- function (df, overall = TRUE, cutoff_dprime = 0, cutoff_2ch
                            c("acc_mean.overall"),
                            c("acc_mean.overall"),
                            c("acc_mean.overall"),
-                           c("dprime.overall"),
+                           c("dprime.sustained", "dprime.impulsive"),
                            c("k.r2b0", "k.r4b0")))
   } else {
     metric_cols %<>%
@@ -173,6 +173,7 @@ post_clean_low_trials <- function (df, min_trials = 5) {
   
   df %<>%
     mutate(filter_cols = map(proc, ~names(.x)[grepl("count", names(.x)) & !grepl("half", names(.x)) & !grepl("correct", names(.x)) & !grepl("cost", names(.x)) & !grepl("start", names(.x)) & !grepl("early", names(.x)) & !grepl("practice", names(.x))]),
+           filter_cols = map_if(filter_cols, module == "SAAT", ~.x[!grepl("overall", .x)], .else = ~.x),
            non_demo_cols = map(proc, ~names(.x)[!(names(.x) %in% get_valid_demos(.x, is_ace = T))]),
            call_filter_cols_bad = map(filter_cols,  ~map_call2_rel("<", .x, min_trials)),
            call_filter_cols_good = map(filter_cols,  ~map_call2_rel(">=", .x, min_trials)))
