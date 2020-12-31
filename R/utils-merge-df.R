75cc1ddd571b5d2639ecc303a5d3339b2ee607e2
monicathieu <mkt2131@columbia.edu>
2020-12-31 10:32:53 -0600

    rm deprecated f, appears never to be called

--- R/utils-merge-df.R
+++ R/utils-merge-df.R
@@ -4,14 +4,3 @@
 multi_merge <- function(df_list = list(), ...) {
   return (Reduce(function(x, y) merge(x, y, all = TRUE, ...), df_list))
 }
-
-#' @importFrom dplyr funs rename_all
-#' @importFrom purrr map2
-#' @keywords internal
-
-flatten_df_list <- function (df_list = list(), keep_prefix = FALSE) {
-  if (keep_prefix) {
-    df_list = map2(df_list, names(df_list), ~rename_all(.x, funs(paste(.y, ., sep = "."))))
-  }
-  return (plyr::rbind.fill(df_list))
-}
