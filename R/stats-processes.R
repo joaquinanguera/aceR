f8b13a1845845cc1d76b0e846211d89dc79cef74
monicathieu <mkt2131@columbia.edu>
2020-12-31 13:13:37 -0600

    delete deprecated f

--- R/stats-processes.R
+++ R/stats-processes.R
@@ -19,15 +19,6 @@ ace_descriptive_statistics <- function(x, col) {
 
 #' @keywords internal 
 
-ace_descriptive_statistics_by_group <- function(x, y) {
-  var_by_group = c(sd = ace_sd_by_group(x, y), se = ace_se_by_group(x, y))
-  avg_by_group = c(mean = ace_mean_by_group(x, y), median = ace_median_by_group(x, y))
-  sum_by_group = c(count = ace_count_by_group(x, y), length = ace_length_by_group(x, y))
-  return (c(var_by_group, avg_by_group, sum_by_group))
-}
-
-#' @keywords internal 
-
 ace_average_turns <- function (x, y) {
   avg_last_3_turns = c(turns_three = ace_turns(x, y, n = 3))
   avg_last_5_turns = c(turns_five = ace_turns(x, y, n = 5))
